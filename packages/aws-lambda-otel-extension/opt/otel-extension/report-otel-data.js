7938c93582a6d74fb593c6361e80400df2466af1
Mariusz Nowak <medyk@medikoo.com>
2022-02-14 14:26:36 +0100

    feat: Support passing metrics data to S3

--- packages/aws-lambda-otel-extension/opt/otel-extension/report-otel-data.js
+++ packages/aws-lambda-otel-extension/opt/otel-extension/report-otel-data.js
@@ -10,11 +10,15 @@ const TRACES_URL = process.env.SLS_OTEL_REPORT_METRICS_URL;
 const EXTRA_REQUEST_HEADERS = process.env.SLS_OTEL_REPORT_REQUEST_HEADERS
   ? Object.fromEntries(new URLSearchParams(process.env.SLS_OTEL_REPORT_REQUEST_HEADERS).entries())
   : {};
+const S3_BUCKET = process.env.SLS_OTEL_REPORT_S3_BUCKET;
 
 const protobuf = REPORT_TYPE === 'proto' ? require('protobufjs') : null;
+// aws-sdk is provided in Lambda runtime
+// eslint-disable-next-line import/no-unresolved
+const s3Client = S3_BUCKET ? new (require('/var/runtime/node_modules/aws-sdk').S3)() : null;
 const fetch = require('node-fetch');
 
-const processData = async (data, { url, protobufPath, protobufType }) => {
+const processData = async (data, { url, s3Key, protobufPath, protobufType }) => {
   if (REPORT_TYPE === 'proto') {
     data = (
       await Promise.all(
@@ -58,6 +62,14 @@ const processData = async (data, { url, protobufPath, protobufType }) => {
         if (!res.ok) console.log(res);
       })
     );
+  } else if (s3Client && s3Key) {
+    await s3Client
+      .putObject({
+        Body: Buffer.from(JSON.stringify(data)),
+        Bucket: S3_BUCKET,
+        Key: s3Key + (REPORT_TYPE === 'proto' ? '.proto' : '.json'),
+      })
+      .promise();
   } else {
     console.log('Report data:', REPORT_TYPE === 'json' ? JSON.stringify(data, null, 2) : data);
   }
@@ -67,12 +79,16 @@ module.exports = {
   metrics: async (data) =>
     processData(data, {
       url: METRICS_URL,
+      // TODO: Once possible, switch to invocation id
+      s3Key: `metrics/${new Date().toISOString()}`,
       protobufPath: '/proto/metric_service.proto',
       protobufType: 'opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest',
     }),
   traces: async (data) =>
     processData(data, {
       url: TRACES_URL,
+      // TODO: Once possible, switch to invocation id
+      s3Key: `traces/${new Date().toISOString()}`,
       protobufPath: '/proto/trace_service.proto',
       protobufType: 'opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest',
     }),
