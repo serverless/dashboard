f17b89033f94d91e85f232c57e22e257ab805511
Mariusz Nowak <medyk@medikoo.com>
2022-02-17 13:24:34 +0100

    test: Configure basic wrapper test

---
+++ packages/aws-lambda-otel-extension/test/fixtures/lambdas/callback-success.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+  callback(null, { statusCode: 200, body: JSON.stringify({ result: 'ok' }) });
+};
---
+++ packages/aws-lambda-otel-extension/test/unit/wrapper.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { expect } = require('chai');
+const path = require('path');
+const { promisify } = require('util');
+const unzip = promisify(require('zlib').unzip);
+const log = require('log').get('test');
+const requireUncached = require('ncjsm/require-uncached');
+const overwriteStdoutWrite = require('process-utils/override-stdout-write');
+
+const lambdaFixturesDirname = path.resolve(__dirname, '../fixtures/lambdas');
+
+describe('wrapper', function () {
+  this.timeout(10000);
+  before(() => {
+    process.env.AWS_LAMBDA_FUNCTION_VERSION = '$LATEST';
+    process.env.AWS_REGION = 'us-east-1';
+    process.env.LAMBDA_TASK_ROOT = lambdaFixturesDirname;
+  });
+
+  it('should handle plain success invocation', async () => {
+    process.env._HANDLER = 'callback-success.handler';
+    process.env.AWS_LAMBDA_FUNCTION_NAME = 'callback-success';
+    let stdoutData = '';
+    await overwriteStdoutWrite(
+      (data) => (stdoutData += data),
+      async () =>
+        requireUncached(async () => {
+          await require('../../opt/otel-extension/wrapper');
+          await new Promise((resolve) => {
+            require(path.resolve(lambdaFixturesDirname, 'callback-success.js')).handler(
+              {},
+              {
+                invokedFunctionArn:
+                  'arn:aws:lambda:us-east-1:123456789012:function:callback-success',
+                getRemainingTimeInMillis: () => 3000,
+              },
+              resolve
+            );
+          });
+        })
+    );
+    const reportLog = stdoutData.split('\n').find((log) => log.startsWith('SERVERLESS_ENTERPRISE'));
+    const reportCompressed = JSON.parse(reportLog.slice(reportLog.indexOf('{'))).b;
+    const report = JSON.parse(String(await unzip(Buffer.from(reportCompressed, 'base64'))));
+    log.debug('result report: %o', report);
+    expect(report.function['telemetry.sdk.language']).to.equal('nodejs');
+    expect(report.function['telemetry.sdk.name']).to.equal('opentelemetry');
+    expect(report.function['faas.name']).to.equal('callback-success');
+    expect(report.function.error).to.equal(false);
+  });
+});
