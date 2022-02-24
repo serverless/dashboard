2fbdb09efbb94e6ece535a505196833dc29a5655
Mariusz Nowak <medyk@medikoo.com>
2022-02-24 16:59:14 +0100

    refactor: Update after first release

--- packages/aws-lambda-otel-extension-dist/scripts/lib/build.js
+++ packages/aws-lambda-otel-extension-dist/scripts/lib/build.js
@@ -3,8 +3,6 @@
 const path = require('path');
 const unlink = require('fs2/unlink');
 const mkdir = require('fs2/mkdir');
-// TODO: Remove next comment after first publication and adding packaage to package.json
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
 const build = require('@serverless/aws-lambda-otel-extension/scripts/lib/build');
 
 const distFilename = path.resolve(__dirname, '../../extension.zip');
