bf28a275a4cb4bdb1bc74e621841383e8c77c2a1
Mariusz Nowak <medyk@medikoo.com>
2022-02-24 15:35:05 +0100

    chore: Temporarily mute eslint warnings

--- packages/aws-lambda-otel-extension-dist/scripts/lib/build.js
+++ packages/aws-lambda-otel-extension-dist/scripts/lib/build.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const unlink = require('fs2/unlink');
 const mkdir = require('fs2/mkdir');
+// TODO: Remove next comment after first publication and adding packaage to package.json
+// eslint-disable-next-line import/no-extraneous-dependencies
 const build = require('@serverless/aws-lambda-otel-extension/scripts/lib/build');
 
 const distFilename = path.resolve(__dirname, '../../extension.zip');
