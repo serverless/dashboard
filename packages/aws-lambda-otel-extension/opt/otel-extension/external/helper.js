7a4f99bfb9fce14c6b60fef822bf442328af7d69
Dan Jarvis <danwakeem@gmail.com>
2022-03-17 14:26:33 -0500

    Fixed version in extension

--- packages/aws-lambda-otel-extension/opt/otel-extension/external/helper.js
+++ packages/aws-lambda-otel-extension/opt/otel-extension/external/helper.js
@@ -107,8 +107,8 @@ const resourceAttributes = [
   },
   {
     key: 'faas.collector_version',
-    value: '@serverless/aws-lambda-otel-extension-0.1.11',
-    source: '@serverless/aws-lambda-otel-extension-0.1.11',
+    value: '@serverless/aws-lambda-otel-extension-0.2.2',
+    source: '@serverless/aws-lambda-otel-extension-0.2.2',
     type: 'stringValue',
   },
 ];
