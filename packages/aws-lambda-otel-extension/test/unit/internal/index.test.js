993f1b40b28e9601e01ad89e8f4c98cc59e4508a
Mariusz Nowak <medyk@medikoo.com>
2022-03-29 17:54:53 +0200

    test: Refactor test logic for success case

--- packages/aws-lambda-otel-extension/test/unit/internal/index.test.js
+++ packages/aws-lambda-otel-extension/test/unit/internal/index.test.js
@@ -12,6 +12,82 @@ const { OTEL_SERVER_PORT } = require('../../../opt/otel-extension/lib/helper');
 
 const lambdaFixturesDirname = path.resolve(__dirname, '../../fixtures/lambdas');
 
+const handleSuccess = async (functionName) => {
+  process.env._HANDLER = `${functionName}.handler`;
+  process.env.AWS_LAMBDA_FUNCTION_NAME = functionName;
+  let stdoutData = '';
+
+  const logsQueue = [];
+  let server;
+  const deferredResultProcessing = new Promise((resolve) => {
+    server = http.createServer((request, response) => {
+      if (request.method === 'POST') {
+        let body = '';
+        request.on('data', (data) => {
+          body += data;
+        });
+        request.on('end', () => {
+          resolve(
+            (async () => {
+              response.writeHead(200, {});
+              response.end('OK');
+              server.close();
+              const data = JSON.parse(body);
+              logsQueue.push(data);
+
+              if (logsQueue.length > 1) {
+                // Validate eventData record for log metadata
+                const logMetadata = logsQueue[0].record;
+                expect(logMetadata.eventData['123']['telemetry.sdk.language']).to.equal('nodejs');
+                expect(logMetadata.eventData['123']['telemetry.sdk.name']).to.equal(
+                  'opentelemetry'
+                );
+                expect(logMetadata.eventData['123']['faas.name']).to.equal(functionName);
+
+                // Validate trace record
+                const reportLog = logsQueue[1].record.split('\t')[2];
+                const reportCompressed = reportLog.slice(reportLog.indexOf('⚡.') + 2);
+                const report = JSON.parse(
+                  String(await unzip(Buffer.from(reportCompressed, 'base64')))
+                );
+                log.debug('result report: %o', report);
+                expect(report.function['telemetry.sdk.language']).to.equal('nodejs');
+                expect(report.function['telemetry.sdk.name']).to.equal('opentelemetry');
+                expect(report.function['faas.name']).to.equal(functionName);
+                expect(report.function.error).to.equal(false);
+              }
+            })()
+          );
+        });
+      }
+    });
+  });
+
+  server.listen(OTEL_SERVER_PORT);
+
+  await overwriteStdoutWrite(
+    (data) => (stdoutData += data),
+    async () =>
+      requireUncached(async () => {
+        await require('../../../opt/otel-extension/internal');
+        await new Promise((resolve) => {
+          require('../../../opt/otel-extension/internal/wrapper').handler(
+            {},
+            {
+              awsRequestId: '123',
+              functionName,
+              invokedFunctionArn: `arn:aws:lambda:us-east-1:123456789012:function:${functionName}`,
+              getRemainingTimeInMillis: () => 3000,
+            },
+            resolve
+          );
+        });
+      })
+  );
+
+  await deferredResultProcessing;
+};
+
 describe('internal', () => {
   before(() => {
     process.env.AWS_LAMBDA_FUNCTION_VERSION = '$LATEST';
@@ -19,80 +95,5 @@ describe('internal', () => {
     process.env.LAMBDA_TASK_ROOT = lambdaFixturesDirname;
   });
 
-  it('should handle plain success invocation', async () => {
-    process.env._HANDLER = 'callback-success.handler';
-    process.env.AWS_LAMBDA_FUNCTION_NAME = 'callback-success';
-    let stdoutData = '';
-
-    const logsQueue = [];
-    let server;
-    const deferredResultProcessing = new Promise((resolve) => {
-      server = http.createServer((request, response) => {
-        if (request.method === 'POST') {
-          let body = '';
-          request.on('data', (data) => {
-            body += data;
-          });
-          request.on('end', () => {
-            resolve(
-              (async () => {
-                response.writeHead(200, {});
-                response.end('OK');
-                server.close();
-                const data = JSON.parse(body);
-                logsQueue.push(data);
-
-                if (logsQueue.length > 1) {
-                  // Validate eventData record for log metadata
-                  const logMetadata = logsQueue[0].record;
-                  expect(logMetadata.eventData['123']['telemetry.sdk.language']).to.equal('nodejs');
-                  expect(logMetadata.eventData['123']['telemetry.sdk.name']).to.equal(
-                    'opentelemetry'
-                  );
-                  expect(logMetadata.eventData['123']['faas.name']).to.equal('callback-success');
-
-                  // Validate trace record
-                  const reportLog = logsQueue[1].record.split('\t')[2];
-                  const reportCompressed = reportLog.slice(reportLog.indexOf('⚡.') + 2);
-                  const report = JSON.parse(
-                    String(await unzip(Buffer.from(reportCompressed, 'base64')))
-                  );
-                  log.debug('result report: %o', report);
-                  expect(report.function['telemetry.sdk.language']).to.equal('nodejs');
-                  expect(report.function['telemetry.sdk.name']).to.equal('opentelemetry');
-                  expect(report.function['faas.name']).to.equal('callback-success');
-                  expect(report.function.error).to.equal(false);
-                }
-              })()
-            );
-          });
-        }
-      });
-    });
-
-    server.listen(OTEL_SERVER_PORT);
-
-    await overwriteStdoutWrite(
-      (data) => (stdoutData += data),
-      async () =>
-        requireUncached(async () => {
-          await require('../../../opt/otel-extension/internal');
-          await new Promise((resolve) => {
-            require('../../../opt/otel-extension/internal/wrapper').handler(
-              {},
-              {
-                awsRequestId: '123',
-                functionName: 'callback-success',
-                invokedFunctionArn:
-                  'arn:aws:lambda:us-east-1:123456789012:function:callback-success',
-                getRemainingTimeInMillis: () => 3000,
-              },
-              resolve
-            );
-          });
-        })
-    );
-
-    await deferredResultProcessing;
-  });
+  it('should handle plain success invocation', async () => handleSuccess('callback-success'));
 });
