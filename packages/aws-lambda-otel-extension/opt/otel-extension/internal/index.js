4821f817098b31f507f18d9ea828e76cca2233d0
Roberto Faga <robertofaga@gmail.com>
2022-04-01 16:25:40 -0300

    Restrict eventData for only current requestId

--- packages/aws-lambda-otel-extension/opt/otel-extension/internal/index.js
+++ packages/aws-lambda-otel-extension/opt/otel-extension/internal/index.js
@@ -335,7 +335,7 @@ const instrumentations = [
       const eventDataPayload = {
         recordType: 'eventData',
         record: {
-          eventData,
+          eventData: { [context.awsRequestId]: eventData[context.awsRequestId] },
           span: {
             traceId: span.spanContext().traceId,
             spanId: span.spanContext().spanId,
