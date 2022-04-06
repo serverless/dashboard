85fbc399ad61914104d7f86e60699042d7fe6f79
Mariusz Nowak <medyk@medikoo.com>
2022-04-06 17:38:58 +0200

    refactor: Prevent misleading "No modules.." warning

--- packages/aws-lambda-otel-extension/opt/otel-extension/internal/aws-lambda-instrumentation.js
+++ packages/aws-lambda-otel-extension/opt/otel-extension/internal/aws-lambda-instrumentation.js
@@ -9,6 +9,17 @@ AwsLambdaInstrumentation.prototype.init = function () {
   }
   // Expose instance for our custom wrapping needs
   AwsLambdaInstrumentation._instance = this;
+  // First part of a patch to prevent "No modules instrumentation has been defined" warning
+  // It is reverted in temporarily overriden `enable()` method invoked right after `init()`
+  return Array(1);
+};
+
+// Second part of patch to prevent "No modules instrumentation has been defined" warning
+AwsLambdaInstrumentation.prototype.enable = function () {
+  // Revert temporary patch (result of dummy response from `init()`)
+  this._modules = [];
+  delete AwsLambdaInstrumentation.prototype.enable;
+  return this.enable();
 };
 
 module.exports = AwsLambdaInstrumentation;
