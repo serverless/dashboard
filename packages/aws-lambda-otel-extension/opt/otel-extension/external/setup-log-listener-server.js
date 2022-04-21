352a112fd1118761d6721cfbec119d1fc4e0e04f
Mariusz Nowak <medyk@medikoo.com>
2022-04-21 20:10:16 +0200

    refactor: Improve handling of dead paths

--- packages/aws-lambda-otel-extension/opt/otel-extension/external/setup-log-listener-server.js
+++ packages/aws-lambda-otel-extension/opt/otel-extension/external/setup-log-listener-server.js
@@ -22,44 +22,40 @@ module.exports = async ({
   // Setup a logs listener server
   const server = http
     .createServer((request, response) => {
-      if (request.method === 'POST') {
-        let body = '';
-        request.on('data', (data) => {
-          body += data;
-        });
-        request.on('end', async () => {
-          try {
-            const batch = JSON.parse(body);
-            logMessage('Current data before write: ', JSON.stringify(logsQueue));
+      if (request.method !== 'POST') throw new Error('Unexpected request');
+      let body = '';
+      request.on('data', (data) => {
+        body += data;
+      });
+      request.on('end', async () => {
+        try {
+          const batch = JSON.parse(body);
+          logMessage('Current data before write: ', JSON.stringify(logsQueue));
 
-            if (batch.length > 0) {
-              const logBatch = batch.filter((log) => log.type === 'platform.report');
-              logsQueue.push(logBatch);
-              writeFileSync(SAVE_FILE, JSON.stringify(logsQueue));
+          if (batch.length > 0) {
+            const logBatch = batch.filter((log) => log.type === 'platform.report');
+            logsQueue.push(logBatch);
+            writeFileSync(SAVE_FILE, JSON.stringify(logsQueue));
 
-              if (callback && logBatch.length > 0) {
-                const reportIds = logBatch.map(
-                  (log) => log.record.requestId || log.record.split('\t')[1]
-                );
-                callback(logsQueue, reportIds);
-              }
+            if (callback && logBatch.length > 0) {
+              const reportIds = logBatch.map(
+                (log) => log.record.requestId || log.record.split('\t')[1]
+              );
+              callback(logsQueue, reportIds);
             }
-
-            const reportedLogs = batch.filter((log) => !log.type.startsWith('platform.'));
-
-            liveLogData.logs.push(...(reportedLogs || []));
-
-            await liveLogCallback();
-          } catch (e) {
-            logMessage('failed to parse logs', e);
           }
-          response.writeHead(200, {});
-          response.end('OK');
-        });
-      } else {
+
+          const reportedLogs = batch.filter((log) => !log.type.startsWith('platform.'));
+
+          liveLogData.logs.push(...(reportedLogs || []));
+
+          await liveLogCallback();
+        } catch (e) {
+          logMessage('failed to parse logs', e);
+        }
         response.writeHead(200, {});
         response.end('OK');
-      }
+      });
     })
     .listen(port, address);
 
