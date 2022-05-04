fa5ae9cc297a63764b6de6b0cbe3897e70c9cb33
Mariusz Nowak <medyk@medikoo.com>
2022-05-04 16:58:05 +0200

    fix: Fix handling of initialization errors

--- packages/aws-lambda-otel-extension/opt/otel-extension/internal/wrapper.js
+++ packages/aws-lambda-otel-extension/opt/otel-extension/internal/wrapper.js
@@ -3,6 +3,12 @@
 process.env._HANDLER = process.env._ORIGIN_HANDLER;
 delete process.env._ORIGIN_HANDLER;
 
+if (!EvalError.$serverlessHandlerFunction) {
+  const handlerError = EvalError.$serverlessHandlerModuleInitializationError;
+  delete EvalError.$serverlessHandlerModuleInitializationError;
+  throw handlerError;
+}
+
 const handlerFunction = EvalError.$serverlessHandlerFunction;
 delete EvalError.$serverlessHandlerFunction;
 
