7ddb26576148f6c5466db00370fc5de770a6507a
Mariusz Nowak <medyk@medikoo.com>
2022-05-24 14:00:05 +0200

    refactor: Log extension processing durations

--- node/packages/aws-lambda-otel-extension/internal/otel-extension-internal-node/wrapper.js
+++ node/packages/aws-lambda-otel-extension/internal/otel-extension-internal-node/wrapper.js
@@ -12,6 +12,86 @@ if (!EvalError.$serverlessHandlerFunction) {
 const handlerFunction = EvalError.$serverlessHandlerFunction;
 delete EvalError.$serverlessHandlerFunction;
 
-module.exports.handler = require('./aws-lambda-instrumentation')._instance._getPatchHandler(
-  handlerFunction
+let requestStartTime;
+let responseStartTime;
+
+const wrappedHandler = require('./aws-lambda-instrumentation')._instance._getPatchHandler(
+  (event, context, callback) => {
+    if (process.env.DEBUG_SLS_OTEL_LAYER) {
+      process._rawDebug(
+        'Extension duration: internal request:',
+        `${Math.round(Number(process.hrtime.bigint() - requestStartTime) / 1000000)}ms`
+      );
+    }
+    let isPromiseResult = false;
+    const originalDone = context.done;
+    const done = (...args) => {
+      if (!isPromiseResult && !responseStartTime) responseStartTime = process.hrtime.bigint();
+      return originalDone(...args);
+    };
+    context.done = done;
+    context.succeed = (result) => done(null, result);
+    context.fail = (err) => done(err == null ? 'handled' : err);
+    const result = handlerFunction(event, context, (...args) => {
+      if (!isPromiseResult && !responseStartTime) responseStartTime = process.hrtime.bigint();
+      return callback(...args);
+    });
+    if (!result) return result;
+    if (typeof result.then !== 'function') return result;
+    isPromiseResult = true;
+    return result.then(
+      (asyncResult) => {
+        if (!responseStartTime) responseStartTime = process.hrtime.bigint();
+        return asyncResult;
+      },
+      (error) => {
+        if (!responseStartTime) responseStartTime = process.hrtime.bigint();
+        throw error;
+      }
+    );
+  }
 );
+
+module.exports.handler = (event, context, callback) => {
+  requestStartTime = process.hrtime.bigint();
+  const logResponseDuration = () => {
+    delete EvalError.$serverlessResponseHandlerPromise;
+    if (process.env.DEBUG_SLS_OTEL_LAYER && responseStartTime) {
+      process._rawDebug(
+        'Extension duration: internal response:',
+        `${Math.round(Number(process.hrtime.bigint() - responseStartTime) / 1000000)}ms`
+      );
+      responseStartTime = null;
+    }
+  };
+  let isPromiseResult = false;
+  const originalDone = context.done;
+  const done = (...args) => {
+    if (!isPromiseResult && EvalError.$serverlessResponseHandlerPromise) {
+      EvalError.$serverlessResponseHandlerPromise.finally(logResponseDuration);
+    }
+    return originalDone(...args);
+  };
+  context.done = done;
+  context.succeed = (result) => done(null, result);
+  context.fail = (err) => done(err == null ? 'handled' : err);
+  const result = wrappedHandler(event, context, (...args) => {
+    if (!isPromiseResult && EvalError.$serverlessResponseHandlerPromise) {
+      EvalError.$serverlessResponseHandlerPromise.finally(logResponseDuration);
+    }
+    return callback(...args);
+  });
+  if (!result) return result;
+  if (typeof result.then !== 'function') return result;
+  isPromiseResult = true;
+  return result.then(
+    (asyncResult) => {
+      logResponseDuration();
+      return asyncResult;
+    },
+    (error) => {
+      logResponseDuration();
+      throw error;
+    }
+  );
+};
