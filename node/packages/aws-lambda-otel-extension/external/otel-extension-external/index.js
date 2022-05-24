ef96e0c563d6207ab6d46bfcb8c433384026fadc
Mariusz Nowak <medyk@medikoo.com>
2022-05-24 15:16:41 +0200

    refactor: Ensure to not propagate debug logs

--- node/packages/aws-lambda-otel-extension/external/otel-extension-external/index.js
+++ node/packages/aws-lambda-otel-extension/external/otel-extension-external/index.js
@@ -70,7 +70,12 @@ module.exports = (async () => {
             response.writeHead(200, {});
             response.end('OK');
             const data = JSON.parse(body);
-            const functionLogEvents = data.filter((event) => event.type === 'function');
+            let functionLogEvents = data.filter((event) => event.type === 'function');
+            if (process.env.DEBUG_SLS_OTEL_LAYER) {
+              functionLogEvents = functionLogEvents.filter(
+                (event) => !event.record.startsWith('Extension duration: ')
+              );
+            }
             if (functionLogEvents.length) {
               const currentRequestData = getCurrentRequestData();
               if (!currentRequestData.eventData) {
