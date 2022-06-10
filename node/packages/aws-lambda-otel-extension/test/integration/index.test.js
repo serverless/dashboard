1162741114e3cc50c136f58019033383397626d3
Mariusz Nowak <medyk@medikoo.com>
2022-06-10 16:46:42 +0200

    test: Process all tested functions simultaneusly

--- node/packages/aws-lambda-otel-extension/test/integration/index.test.js
+++ node/packages/aws-lambda-otel-extension/test/integration/index.test.js
@@ -3,24 +3,16 @@
 const { expect } = require('chai');
 
 const path = require('path');
-const wait = require('timers-ext/promise/sleep');
-const { CloudWatchLogs } = require('@aws-sdk/client-cloudwatch-logs');
-const { Lambda } = require('@aws-sdk/client-lambda');
 const log = require('log').get('test');
-const resolveDirZipBuffer = require('../utils/resolve-dir-zip-buffer');
 const normalizeOtelAttributes = require('../utils/normalize-otel-attributes');
-const ensureNpmDependencies = require('../../scripts/lib/ensure-npm-dependencies');
-const awsRequest = require('./aws-request');
 const basename = require('./basename');
 const cleanup = require('./cleanup');
 const createCoreResources = require('./create-core-resources');
-
-const fixturesDirname = path.resolve(__dirname, '../fixtures/lambdas');
+const processFunction = require('./process-function');
 
 describe('integration', function () {
   this.timeout(120000);
-  const config = {};
-  let lambdasCodeZipBuffer;
+  const coreConfig = {};
 
   const functionsConfig = new Map([
     ['success-callback', true],
@@ -103,214 +95,81 @@ describe('integration', function () {
     ],
   ]);
 
-  const processFunction = async (handlerModuleName, options = {}) => {
-    const startTime = Date.now();
-    const payload = options.payload || { foo: 'bar' };
-    const functionBasename = handlerModuleName.includes(path.sep)
-      ? path.dirname(handlerModuleName)
-      : handlerModuleName;
-    const functionName = `${basename}-${functionBasename}`;
-    const ensureIsActive = async () => {
-      const {
-        Configuration: { State: state },
-      } = await awsRequest(Lambda, 'getFunction', { FunctionName: functionName });
-      if (state !== 'Active') await ensureIsActive();
-    };
-    const invokeFunction = async () => {
-      const result = await awsRequest(Lambda, 'invoke', {
-        FunctionName: functionName,
-        Payload: Buffer.from(JSON.stringify(payload), 'utf8'),
-      });
-      try {
-        const responsePayload = JSON.parse(Buffer.from(result.Payload));
-        log.debug('invoke payload %O', responsePayload);
-        log.debug('invoke parsed payload %O', JSON.parse(responsePayload.body));
-      } catch {
-        /* ignore */
-      }
-      if (result.FunctionError) {
-        if (options.isFailure) return;
-        throw new Error(`Invocation errored: ${result.FunctionError}`);
-      }
-    };
-    const deleteFunction = async () => {
-      await awsRequest(Lambda, 'deleteFunction', { FunctionName: functionName });
-    };
-    const resolveReportEvents = async () => {
-      try {
-        return (
-          await awsRequest(CloudWatchLogs, 'filterLogEvents', {
-            startTime,
-            logGroupName: `/aws/lambda/${functionName}`,
-          })
-        ).events;
-      } catch (error) {
-        if (error.name === 'ResourceNotFoundException') {
-          log.info('log group not ready, wait and retry %s', functionBasename);
-          await wait(1000);
-          return resolveReportEvents();
-        }
-        throw error;
-      }
-    };
-
-    log.info('Ensure function is active %s', functionBasename);
-    await ensureIsActive();
-    log.info('Invoke function #1 %s', functionBasename);
-    await invokeFunction();
-    log.info('Invoke function #2 %s', functionBasename);
-    await invokeFunction();
-
-    let reports;
-    do {
-      log.info('Retrieve list of written reports %s', functionBasename);
-      const events = await resolveReportEvents();
-      reports = [];
-      let currentInvocationReports = [];
-      for (const { message } of events) {
-        if (message.startsWith('⚡')) {
-          const reportType = message.slice(2, message.indexOf(':'));
-          if (reportType === 'logs') continue;
-          currentInvocationReports.push([
-            reportType,
-            JSON.parse(message.slice(message.indexOf(':') + 1).trim()),
-          ]);
-          continue;
-        }
-        if (message.startsWith('REPORT RequestId: ')) {
-          reports.push(currentInvocationReports);
-          currentInvocationReports = [];
-        }
-      }
-    } while (reports.length < 2);
-
-    log.info('Delete function %s', functionBasename);
-    await deleteFunction();
-
-    return reports;
-  };
+  let deferredResults;
 
   before(async () => {
-    ensureNpmDependencies('test/fixtures/lambdas');
-    const createFunctions = async () => {
-      await Promise.all(
-        Array.from(functionsConfig, async function self([handlerModuleName, { createOptions }]) {
-          if (!createOptions) createOptions = {};
-          const functionBasename = handlerModuleName.includes(path.sep)
-            ? path.dirname(handlerModuleName)
-            : handlerModuleName;
-          const functionName = `${basename}-${functionBasename}`;
-          log.info('Create function %s', functionBasename);
-          try {
-            await awsRequest(Lambda, 'createFunction', {
-              Handler: `${handlerModuleName}.handler`,
-              Role: config.roleArn,
-              Runtime: 'nodejs14.x',
-              ...createOptions.configuration,
-              Code: {
-                ZipFile: lambdasCodeZipBuffer,
-              },
-              FunctionName: functionName,
-              Layers: [config.layerArn],
-              Environment: {
-                Variables: {
-                  AWS_LAMBDA_EXEC_WRAPPER: '/opt/otel-extension-internal-node/exec-wrapper.sh',
-                  DEBUG_SLS_OTEL_LAYER: '1',
-                  SLS_OTEL_USER_SETTINGS: JSON.stringify({
-                    metrics: { outputType: 'json' },
-                    traces: { outputType: 'json' },
-                  }),
-                },
-              },
-            });
-          } catch (error) {
-            if (
-              error.message.includes(
-                'The role defined for the function cannot be assumed by Lambda'
-              ) ||
-              error.message.includes('because the KMS key is invalid for CreateGrant')
-            ) {
-              // Occassional race condition issue on AWS side, retry
-              await self([handlerModuleName, { createOptions }]);
-              return;
-            }
-            if (error.message.includes('Function already exist')) {
-              log.notice('Function %s already exists, deleting and re-creating', functionBasename);
-              await awsRequest(Lambda, 'deleteFunction', { FunctionName: functionName });
-              await self([handlerModuleName, { createOptions }]);
-              return;
-            }
-            throw error;
-          }
-        })
-      );
-    };
-
-    [lambdasCodeZipBuffer] = await Promise.all([
-      resolveDirZipBuffer(fixturesDirname),
-      createCoreResources(config),
-    ]);
-    await createFunctions();
+    await createCoreResources(coreConfig);
+    deferredResults = new Map(
+      Array.from(functionsConfig, ([handlerModuleName, testConfig]) => [
+        handlerModuleName,
+        processFunction(handlerModuleName, testConfig, coreConfig).catch((error) => ({
+          // As we process result promises sequentially step by step in next turn, allowing them to
+          // reject will generate unhandled rejection.
+          // Therefore this scenario is converted to successuful { error } resolution
+          error,
+        })),
+      ])
+    );
   });
 
   for (const [handlerModuleName, { invokeOptions = {}, test }] of functionsConfig) {
-    const functionBasename = handlerModuleName.includes(path.sep)
+    const functionBasename = handlerModuleName.includes('/')
       ? path.dirname(handlerModuleName)
       : handlerModuleName;
-    describe(functionBasename, () => {
-      let reports;
-      before(async () => {
-        reports = await processFunction(handlerModuleName, invokeOptions);
-        log.info('retrieved reposts %o', reports);
-      });
-      it('test', () => {
-        if (!invokeOptions.isFailure) {
-          // Current timeout handling is unreliable, therefore do not attempt to confirm
-          // on all reports
 
-          // While reports should come in order as specified below,
-          // there were observed cases when it wasn't the case,
-          // e.g. telemetryData (response) was received before eventData (request)
-          expect(
-            reports.map((invocationReports) => invocationReports.map(([type]) => type).sort())
-          ).to.deep.equal([
-            ['request', 'response', 'metrics', 'traces'].sort(),
-            ['metrics', 'request', 'response', 'metrics', 'traces'].sort(),
-          ]);
-        }
-        const allReports = reports.flat();
-        const metricsReport = allReports.find(([reportType]) => reportType === 'metrics')[1];
-        const tracesReport = allReports.find(([reportType]) => reportType === 'traces')[1];
-        const resourceMetrics = normalizeOtelAttributes(
-          metricsReport.resourceMetrics[0].resource.attributes
-        );
-        expect(resourceMetrics['faas.name']).to.equal(`${basename}-${functionBasename}`);
-        const resourceSpans = normalizeOtelAttributes(
-          tracesReport.resourceSpans[0].resource.attributes
-        );
-        expect(resourceSpans['faas.name']).to.equal(`${basename}-${functionBasename}`);
+    it(functionBasename, async () => {
+      const functionResult = await deferredResults.get(handlerModuleName);
+      if (functionResult.error) throw functionResult.error;
+      const { reports } = functionResult;
+      if (!invokeOptions.isFailure) {
+        // Current timeout handling is unreliable, therefore do not attempt to confirm
+        // on all reports
 
-        const instrumentationSpans = {};
-        for (const {
-          instrumentationLibrary: { name },
-          spans,
-        } of tracesReport.resourceSpans[0].instrumentationLibrarySpans) {
-          instrumentationSpans[name] = spans.map((span) => ({
-            ...span,
-            attributes: normalizeOtelAttributes(span.attributes),
-          }));
-        }
-        log.debug('instrumentationSpans %o', instrumentationSpans);
-        if (test) {
-          test({
-            metricsReport,
-            tracesReport,
-            resourceMetrics,
-            resourceSpans,
-            instrumentationSpans,
-          });
-        }
-      });
+        // While reports should come in order as specified below,
+        // there were observed cases when it wasn't the case,
+        // e.g. telemetryData (response) was received before eventData (request)
+        expect(
+          reports.map((invocationReports) => invocationReports.map(([type]) => type).sort())
+        ).to.deep.equal([
+          ['request', 'response', 'metrics', 'traces'].sort(),
+          ['metrics', 'request', 'response', 'metrics', 'traces'].sort(),
+        ]);
+      }
+      const allInvocationReports = reports.flat();
+      const metricsReport = allInvocationReports.find(
+        ([reportType]) => reportType === 'metrics'
+      )[1];
+
+      const tracesReport = allInvocationReports.find(([reportType]) => reportType === 'traces')[1];
+      const resourceMetrics = normalizeOtelAttributes(
+        metricsReport.resourceMetrics[0].resource.attributes
+      );
+      expect(resourceMetrics['faas.name']).to.equal(`${basename}-${functionBasename}`);
+      const resourceSpans = normalizeOtelAttributes(
+        tracesReport.resourceSpans[0].resource.attributes
+      );
+      expect(resourceSpans['faas.name']).to.equal(`${basename}-${functionBasename}`);
+
+      const instrumentationSpans = {};
+      for (const {
+        instrumentationLibrary: { name },
+        spans,
+      } of tracesReport.resourceSpans[0].instrumentationLibrarySpans) {
+        instrumentationSpans[name] = spans.map((span) => ({
+          ...span,
+          attributes: normalizeOtelAttributes(span.attributes),
+        }));
+      }
+      log.debug('instrumentationSpans %o', instrumentationSpans);
+      if (test) {
+        test({
+          metricsReport,
+          tracesReport,
+          resourceMetrics,
+          resourceSpans,
+          instrumentationSpans,
+        });
+      }
     });
   }
 
---
+++ node/packages/aws-lambda-otel-extension/test/integration/process-function.js
@@ -0,0 +1,163 @@
+'use strict';
+
+const path = require('path');
+const log = require('log').get('test');
+const { CloudWatchLogs } = require('@aws-sdk/client-cloudwatch-logs');
+const { Lambda } = require('@aws-sdk/client-lambda');
+const wait = require('timers-ext/promise/sleep');
+const basename = require('./basename');
+const awsRequest = require('./aws-request');
+const resolveDirZipBuffer = require('../utils/resolve-dir-zip-buffer');
+const ensureNpmDependencies = require('../../scripts/lib/ensure-npm-dependencies');
+
+const fixturesDirname = path.resolve(__dirname, '../fixtures/lambdas');
+
+const create = async (fnConfig, coreConfig, testConfig) => {
+  const { createOptions = {} } = testConfig;
+  try {
+    await awsRequest(Lambda, 'createFunction', {
+      Handler: `${fnConfig.handlerModuleName}.handler`,
+      Role: coreConfig.roleArn,
+      Runtime: 'nodejs14.x',
+      ...createOptions.configuration,
+      Code: {
+        ZipFile: fnConfig.codeZipBuffer,
+      },
+      FunctionName: fnConfig.name,
+      Layers: [coreConfig.layerArn],
+      Environment: {
+        Variables: {
+          AWS_LAMBDA_EXEC_WRAPPER: '/opt/otel-extension-internal-node/exec-wrapper.sh',
+          DEBUG_SLS_OTEL_LAYER: '1',
+          SLS_OTEL_USER_SETTINGS: JSON.stringify({
+            metrics: { outputType: 'json' },
+            traces: { outputType: 'json' },
+          }),
+        },
+      },
+    });
+  } catch (error) {
+    if (
+      error.message.includes('The role defined for the function cannot be assumed by Lambda') ||
+      error.message.includes('because the KMS key is invalid for CreateGrant')
+    ) {
+      // Occassional race condition issue on AWS side, retry
+      await create(fnConfig, coreConfig, testConfig);
+      return;
+    }
+    if (error.message.includes('Function already exist')) {
+      log.notice('Function %s already exists, deleting and re-creating', fnConfig.basename);
+      await awsRequest(Lambda, 'deleteFunction', { FunctionName: fnConfig.name });
+      await create(fnConfig, coreConfig, testConfig);
+      return;
+    }
+    throw error;
+  }
+};
+
+const ensureIsActive = async (fnConfig) => {
+  const {
+    Configuration: { State: state },
+  } = await awsRequest(Lambda, 'getFunction', { FunctionName: fnConfig.name });
+  if (state !== 'Active') await ensureIsActive(fnConfig);
+};
+
+const invoke = async (fnConfig, testConfig) => {
+  // Local computer time might a bit out of sync. Relax time range by 3 seconds
+  fnConfig.invokeStartTime = Date.now() - 3000;
+  const { invokeOptions = {} } = testConfig;
+
+  const result = await awsRequest(Lambda, 'invoke', {
+    FunctionName: fnConfig.name,
+    Payload: Buffer.from(JSON.stringify(invokeOptions.payload || {}), 'utf8'),
+  });
+  try {
+    const responsePayload = JSON.parse(Buffer.from(result.Payload));
+    log.debug('invoke payload %O', responsePayload);
+    log.debug('invoke parsed payload %O', JSON.parse(responsePayload.body));
+  } catch {
+    /* ignore */
+  }
+  if (result.FunctionError) {
+    if (invokeOptions.isFailure) return;
+    throw new Error(`Invocation errored: ${result.FunctionError}`);
+  }
+};
+
+const deleteFunction = async (fnConfig) => {
+  await awsRequest(Lambda, 'deleteFunction', { FunctionName: fnConfig.name });
+};
+
+const retrieveReports = async (fnConfig) => {
+  const retrieveReportEvents = async () => {
+    try {
+      return (
+        await awsRequest(CloudWatchLogs, 'filterLogEvents', {
+          startTime: fnConfig.invokeStartTime,
+          logGroupName: `/aws/lambda/${fnConfig.name}`,
+        })
+      ).events;
+    } catch (error) {
+      if (error.name === 'ResourceNotFoundException') {
+        log.info('log group not ready, wait and retry %s', fnConfig.basename);
+        await wait(1000);
+        return retrieveReportEvents();
+      }
+      throw error;
+    }
+  };
+
+  let reports;
+  do {
+    const events = await retrieveReportEvents();
+    reports = [];
+    let currentInvocationReports = [];
+    for (const { message } of events) {
+      if (message.startsWith('⚡')) {
+        const reportType = message.slice(2, message.indexOf(':'));
+        if (reportType === 'logs') continue;
+        currentInvocationReports.push([
+          reportType,
+          JSON.parse(message.slice(message.indexOf(':') + 1).trim()),
+        ]);
+        continue;
+      }
+      if (message.startsWith('REPORT RequestId: ')) {
+        reports.push(currentInvocationReports);
+        currentInvocationReports = [];
+      }
+    }
+  } while (reports.length < 2);
+
+  log.info('Obtained reports %s %o', fnConfig.basename, reports);
+  return reports;
+};
+
+module.exports = async (handlerModuleName, testConfig, coreConfig) => {
+  ensureNpmDependencies('test/fixtures/lambdas');
+  const fnConfig = {
+    handlerModuleName,
+    basename: handlerModuleName.includes('/') ? path.dirname(handlerModuleName) : handlerModuleName,
+    codeZipBuffer: await resolveDirZipBuffer(fixturesDirname),
+  };
+  fnConfig.name = `${basename}-${fnConfig.basename}`;
+
+  log.info('Create function %s', fnConfig.basename);
+  await create(fnConfig, coreConfig, testConfig);
+
+  log.info('Ensure function is active %s', fnConfig.basename);
+  await ensureIsActive(fnConfig);
+
+  fnConfig.invokeStartTime = Date.now();
+  log.info('Invoke function #1 %s', fnConfig.basename);
+  await invoke(fnConfig, testConfig);
+
+  log.info('Invoke function #2 %s', fnConfig.basename);
+  await invoke(fnConfig, testConfig);
+
+  log.info('Delete function %s', fnConfig.basename);
+  await deleteFunction(fnConfig);
+
+  log.info('Retrieve list of written reports %s', fnConfig.basename);
+  return { reports: await retrieveReports(fnConfig) };
+};
