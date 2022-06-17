98a2ec3c7b493e70bf1e982521830e9a54864c43
Shane Spencer <305301+whardier@users.noreply.github.com>
2022-06-17 04:34:02 +0000

    remove some external extension variables and threading tools

--- python/packages/aws-lambda-otel-extension/src/serverless/aws_lambda_otel_extension/shared/synchronization.py
+++ python/packages/aws-lambda-otel-extension/src/serverless/aws_lambda_otel_extension/shared/synchronization.py
@@ -1,9 +1,3 @@
 import threading
 
-otel_server_active_event = threading.Event()
-log_server_active_event = threading.Event()
-
-extension_registered_event = threading.Event()
-log_registered_event = threading.Event()
-
 extension_theading_lock = threading.Lock()
