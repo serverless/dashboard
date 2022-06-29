1d0317fc6d6b293e96ec01867da686a35abfed37
Shane Spencer <305301+whardier@users.noreply.github.com>
2022-06-29 05:16:58 +0000

    new build script leveraging docker to produce byte compiled output and universal/split layers based on arch

--- python/packages/aws-lambda-otel-extension/scripts/build-layer.py
+++ python/packages/aws-lambda-otel-extension/scripts/build-layer.py
@@ -1,196 +1,238 @@
-#!/usr/bin/env python3.9
+# Essentially just a fancy Makefile.. or maybe less fancy Makefile.. matter of perspective.
 
 import argparse
-import logging
-import pathlib
-import shutil
-import subprocess
-import sys
-import time
-import zipfile
+import os
 from collections import Counter
+from pathlib import PosixPath
+from shutil import rmtree
+from subprocess import call
+from textwrap import dedent
+from typing import Dict, Optional
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("build")
+UID = os.getuid()
+GID = os.getgid()
 
-BUILD_MATRIX = {
-    # "3.6": ["manylinux_2_17_x86_64"], # Dies AUG 2022
-    "3.7": ["manylinux_2_17_x86_64"],
-    "3.8": ["manylinux_2_17_x86_64", "manylinux_2_17_aarch64"],
-    "3.9": ["manylinux_2_17_x86_64", "manylinux_2_17_aarch64"],
-    # Waiting for glib2.6 wheels to appear for wrapt/grpcio
-    # "3.8": ["manylinux_2_26_x86_64", "manylinux_2_26_aarch64"],
-    # "3.9": ["manylinux_2_26_x86_64", "manylinux_2_26_aarch64"],
+PROJECT_PATH = PosixPath(__file__).parent.parent.absolute()
+DOCKER_PROJECT_PATH = PosixPath("/project")
+
+PYTHON_VERSION_ARCH_DOCKER_IMAGE_MATRIX = {
+    "3.6": {
+        "amd64": "public.ecr.aws/sam/build-python3.6:latest-x86_64",
+    },
+    "3.7": {
+        "amd64": "public.ecr.aws/sam/build-python3.7:latest-x86_64",
+    },
+    "3.8": {
+        "amd64": "public.ecr.aws/sam/build-python3.8:latest-x86_64",
+        "arm64": "public.ecr.aws/sam/build-python3.8:latest-arm64",
+    },
+    "3.9": {
+        "amd64": "public.ecr.aws/sam/build-python3.9:latest-x86_64",
+        "arm64": "public.ecr.aws/sam/build-python3.9:latest-arm64",
+    },
 }
 
-PYTHON_VERSION_EXPANSION = {
-    # "3.6": "3.6.15",
-    "3.7": "3.7.13",
-    "3.8": "3.8.13",
-    "3.9": "3.9.11",
-}
 
-BUILD_PATH_NAME = "build"
-DIST_PATH_NAME = "dist"
-EXTENSION_PATH_NAME = "extension"
-INTERNAL_PATH_NAME = "internal"
-MATRIX_PATH_NAME = "matrix"
-PYTHON_PATH_NAME = "python"
-LIB_PATH_NAME = "lib"
-SRC_PATH_NAME = "src"
-ASSET_PATH_NAME = "assets"
+class BuildLayerNamespace(argparse.Namespace):
+    build_path: PosixPath
+    dist_path: PosixPath
+    docker_bin_path: Optional[PosixPath]
 
-
-DIST_EXTENSION_PATH_NAME = "extension.zip"
-
-PROJECT_SCRIPTS_PATH = pathlib.Path(__file__).parent.relative_to(pathlib.Path("."))
-PROJECT_PATH = PROJECT_SCRIPTS_PATH.parent
-PROJECT_SRC_PATH = PROJECT_PATH.joinpath(SRC_PATH_NAME)
-ASSETS_PATH = PROJECT_PATH.joinpath(ASSET_PATH_NAME)
-ASSETS_INTERNAL_PATH = ASSETS_PATH.joinpath(INTERNAL_PATH_NAME)
-
-BUILD_PATH = PROJECT_PATH.joinpath(BUILD_PATH_NAME)
-BUILD_MATRIX_PATH = BUILD_PATH.joinpath(MATRIX_PATH_NAME)
-
-BUILD_EXTENSION_PATH = BUILD_PATH.joinpath(EXTENSION_PATH_NAME)
-BUILD_EXTENSION_PYTHON_PATH = BUILD_EXTENSION_PATH.joinpath(PYTHON_PATH_NAME)
-
-DIST_PATH = PROJECT_PATH.joinpath(DIST_PATH_NAME)
-DIST_PATH_EXTENSION_INTERNAL_ZIP_PATH = DIST_PATH.joinpath(DIST_EXTENSION_PATH_NAME)
-
-
-class ArgumentNamespace(argparse.Namespace):
     skip_download: bool
     skip_install: bool
-    no_clean: bool
+
+    single_arch: bool
 
 
-def main():
+def setup_build_environment(args: BuildLayerNamespace):
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--skip-download", action="store_true")
-    parser.add_argument("--skip-install", action="store_true")
-    parser.add_argument("--no-clean", action="store_true")
+    args.build_path.mkdir(parents=True, exist_ok=True)
+    args.dist_path.mkdir(parents=True, exist_ok=True)
 
-    args = parser.parse_args(namespace=ArgumentNamespace())
+    tmp_path = args.build_path / "tmp"
+    tmp_path.mkdir(parents=True, exist_ok=True)
 
-    if not args.no_clean:
-        if not args.skip_download:
-            if BUILD_MATRIX_PATH.exists():
-                logger.debug("rmtree:%s", BUILD_MATRIX_PATH)
-                shutil.rmtree(BUILD_MATRIX_PATH)
+    cache_path = args.build_path / "cache"
+    cache_path.mkdir(parents=True, exist_ok=True)
 
-        if BUILD_EXTENSION_PATH.exists():
-            logger.debug("rmtree:%s", BUILD_EXTENSION_PATH)
-            shutil.rmtree(str(BUILD_EXTENSION_PATH))
+    downloads_path = args.build_path / "downloads"
 
-    # Ensure that the build/dist directories exists.
-    BUILD_PATH.mkdir(exist_ok=True)
-    DIST_PATH.mkdir(exist_ok=True)
+    if not args.skip_download:
+        if downloads_path.exists():
+            rmtree(downloads_path, ignore_errors=True)
 
-    logger.debug("copytree:%s:%s", ASSETS_INTERNAL_PATH, BUILD_EXTENSION_PATH)
-    shutil.copytree(str(ASSETS_INTERNAL_PATH), str(BUILD_EXTENSION_PATH), copy_function=shutil.copy2)
-
-    BUILD_EXTENSION_PATH.joinpath(".serverless_aws_lambda_otel_extension").write_text(str(time.time()))
-
-    matrix_counter = 0
-
-    for (
-        matrix_python_version,
-        matrix_platforms,
-    ) in BUILD_MATRIX.items():
-        expanded_matrix_python_version = PYTHON_VERSION_EXPANSION[matrix_python_version]
-        for matrix_platform in matrix_platforms:
-            matrix_counter += 1
-            target_path = BUILD_MATRIX_PATH.joinpath(
-                matrix_python_version,
-                matrix_platform,
-            )
-            target_path.mkdir(parents=True, exist_ok=True)
-
-            if not args.skip_download:
-                logger.debug("pip:downloading:target:%s", target_path)
-
-                subprocess.check_output(
-                    [
-                        "pip",
-                        "download",
-                        "--disable-pip-version-check",
-                        f"--platform={matrix_platform}",
-                        f"--python-version={expanded_matrix_python_version}",
-                        "--only-binary=:all:",
-                        str(PROJECT_PATH),
-                        "-d",
-                        str(target_path),
-                    ]
-                )
+    install_path = args.build_path / "install"
 
     if not args.skip_install:
-        logger.debug("pip:install:target:%s", BUILD_EXTENSION_PYTHON_PATH)
-        subprocess.check_output(
-            [
-                "pip",
-                "install",
-                "--disable-pip-version-check",
-                str(PROJECT_PATH),
-                "--no-deps",
-                "-t",
-                str(BUILD_EXTENSION_PYTHON_PATH),
-            ]
-        )
+        if install_path.exists():
+            rmtree(install_path, ignore_errors=True)
 
-    # Get a count of all the wheels in the build packages directories
-    wheel_path_name_counts = Counter([wheel_path.name for wheel_path in BUILD_MATRIX_PATH.glob("**/*.whl")])
 
-    uncommon_wheel_path_names = []
+def docker_download_wheels_from_matrix(args: BuildLayerNamespace):
 
-    for wheel_path_name, count in wheel_path_name_counts.items():
-        if count != matrix_counter:
-            uncommon_wheel_path_names.append(wheel_path_name)
+    docker = str(args.docker_bin_path.absolute()) if args.docker_bin_path else "docker"
 
-    for (
-        matrix_python_version,
-        matrix_platforms,
-    ) in BUILD_MATRIX.items():
-        for matrix_platform in matrix_platforms:
-            target_path = BUILD_MATRIX_PATH.joinpath(
-                matrix_python_version,
-                matrix_platform,
+    for python_version, arch_docker_images in sorted(PYTHON_VERSION_ARCH_DOCKER_IMAGE_MATRIX.items()):
+        for arch, docker_image in arch_docker_images.items():
+            docker_cache_path = DOCKER_PROJECT_PATH / "build" / "cache"
+            docker_downloads_path = DOCKER_PROJECT_PATH / "build" / "downloads" / arch / python_version
+            call(
+                [
+                    *[docker, "run", "--platform", f"linux/{arch}", "--user", f"{UID}:{GID}", "--rm", "-it"],
+                    *["-v", f"{PROJECT_PATH}:{DOCKER_PROJECT_PATH}"],
+                    docker_image,
+                    *[
+                        "/bin/bash",
+                        "-c",
+                        dedent(
+                            f"""
+                            set -ex
+                            python{python_version} -m venv /tmp/.venv \
+                                && source /tmp/.venv/bin/activate
+                            python{python_version} -m pip --cache-dir {docker_cache_path} install \
+                                --upgrade pip setuptools wheel
+                            python{python_version} -m pip --cache-dir {docker_cache_path} download \
+                                --prefer-binary /project \
+                                --dest {docker_downloads_path}
+                        """,
+                        ),
+                    ],
+                ],
             )
 
-            target_path.mkdir(parents=True, exist_ok=True)
 
-            for wheel_path in target_path.glob("*.whl"):
-                extraction_path = BUILD_EXTENSION_PYTHON_PATH
+def docker_install_wheels_from_matrix(args: BuildLayerNamespace):
 
-                if wheel_path.name in uncommon_wheel_path_names:
-                    extraction_path = BUILD_EXTENSION_PYTHON_PATH.joinpath(
-                        LIB_PATH_NAME,
-                        f"{PYTHON_PATH_NAME}{matrix_python_version}",
-                        "site-packages",
-                    )
+    docker = str(args.docker_bin_path.absolute()) if args.docker_bin_path else "docker"
 
-                extraction_path.mkdir(parents=True, exist_ok=True)
+    tmp_path = args.build_path / "tmp"
 
-                with zipfile.ZipFile(str(wheel_path)) as zip_file:
-                    logger.debug("unpacking:%s", wheel_path)
-                    zip_file.extractall(str(extraction_path))
+    downloads_path = args.build_path / "downloads"
 
-    logger.debug("unlink:%s", DIST_PATH_EXTENSION_INTERNAL_ZIP_PATH)
+    arch_counter: Counter = Counter()
+    file_counters_by_arch: Dict[str, Counter] = {}
 
-    try:
-        DIST_PATH_EXTENSION_INTERNAL_ZIP_PATH.unlink()
-    except:
-        pass
+    # Aggregate all the file names into a counter under an arch key.  This is just a not-so-fancy way to quickly sum up
+    # how many duplicate files there are for each arch and then optionally install ones that cover every python version
+    # in `/opt/python` directory and the rest will go into the respective `/opt/python/lib/python3.x/site-packages`
+    # directory.
+    for python_version, arch_docker_images in sorted(PYTHON_VERSION_ARCH_DOCKER_IMAGE_MATRIX.items()):
+        for arch in arch_docker_images.keys():
 
-    with zipfile.ZipFile(DIST_PATH_EXTENSION_INTERNAL_ZIP_PATH, "w") as zip_file:
-        logger.debug("packing:%s", DIST_PATH_EXTENSION_INTERNAL_ZIP_PATH)
-        for extension_file_path in BUILD_EXTENSION_PATH.glob("**/*"):
-            zip_file.write(
-                str(extension_file_path),
-                arcname=str(extension_file_path.relative_to(BUILD_EXTENSION_PATH)),
+            if args.single_arch:
+                arch_or_universal = arch
+            else:
+                arch_or_universal = "universal"
+
+            arch_counter.update({arch_or_universal: 1})
+
+            downloads_arch_python_version_path = downloads_path / arch / python_version
+
+            file_counters_by_arch.setdefault(arch_or_universal, Counter()).update(
+                [f.name for f in downloads_arch_python_version_path.glob("*") if f.is_file()]
+            )
+
+    for python_version, arch_docker_images in sorted(PYTHON_VERSION_ARCH_DOCKER_IMAGE_MATRIX.items()):
+        for arch, docker_image in arch_docker_images.items():
+
+            if args.single_arch:
+                arch_or_universal = arch
+            else:
+                arch_or_universal = "universal"
+
+            docker_cache_path = DOCKER_PROJECT_PATH / "build" / "cache"
+
+            docker_tmp_path = DOCKER_PROJECT_PATH / "build" / "tmp"
+
+            docker_common_install_path = (
+                DOCKER_PROJECT_PATH / "build" / "install" / arch_or_universal / "opt" / "python"
+            )
+            docker_uncommon_install_path = (
+                docker_common_install_path / "lib" / f"python{python_version}" / "site-packages"
+            )
+
+            downloads_arch_python_version_path = downloads_path / arch / python_version
+
+            arch_count = arch_counter[arch_or_universal]
+
+            common_dependencies = [
+                DOCKER_PROJECT_PATH / f.relative_to(PROJECT_PATH)
+                for f in downloads_arch_python_version_path.glob("*")
+                if f.is_file() and file_counters_by_arch[arch_or_universal][f.name] == arch_count
+            ]
+
+            uncommon_dependencies = [
+                DOCKER_PROJECT_PATH / f.relative_to(PROJECT_PATH)
+                for f in downloads_arch_python_version_path.glob("*")
+                if f.is_file() and file_counters_by_arch[arch_or_universal][f.name] < arch_count
+            ]
+
+            (tmp_path / "requirements.common.txt").write_text(
+                os.linesep.join(str(f) for f in common_dependencies) + os.linesep
+            )
+            (tmp_path / "requirements.uncommon.txt").write_text(
+                os.linesep.join(str(f) for f in uncommon_dependencies) + os.linesep
+            )
+
+            docker_common_requirements_path = docker_tmp_path / "requirements.common.txt"
+            docker_uncommon_requirements_path = docker_tmp_path / "requirements.uncommon.txt"
+
+            call(
+                [
+                    *[docker, "run", "--platform", f"linux/{arch}", "--user", f"{UID}:{GID}", "--rm", "-it"],
+                    *["-v", f"{PROJECT_PATH}:/project"],
+                    docker_image,
+                    *[
+                        "/bin/bash",
+                        "-c",
+                        dedent(
+                            f"""
+                            set -ex
+                            python{python_version} -m venv /tmp/.venv \
+                                && source /tmp/.venv/bin/activate
+                            python{python_version} -m pip --cache-dir {docker_cache_path} install \
+                                --upgrade pip setuptools wheel
+                            python{python_version} -m pip --cache-dir {docker_cache_path} install \
+                                --upgrade --force-reinstall \
+                                --requirement {docker_common_requirements_path} \
+                                --target {docker_common_install_path} \
+                                --no-deps
+                            python{python_version} -m pip --cache-dir {docker_cache_path} install \
+                                --upgrade --force-reinstall  \
+                                --requirement {docker_uncommon_requirements_path} \
+                                --target {docker_uncommon_install_path} \
+                                --no-deps
+                            python{python_version} -m pip --cache-dir {docker_cache_path} install \
+                                --upgrade --force-reinstall \
+                                {DOCKER_PROJECT_PATH} \
+                                --target {docker_common_install_path} \
+                                --no-deps
+                        """,
+                        ),
+                    ],
+                ],
             )
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--build-path", type=PosixPath, default=PROJECT_PATH / "build")
+    parser.add_argument("--dist-path", type=PosixPath, default=PROJECT_PATH / "dist")
+    parser.add_argument("--docker-bin-path", type=PosixPath)
+
+    parser.add_argument("--skip-download", action="store_true")
+    parser.add_argument("--skip-install", action="store_true")
+
+    parser.add_argument("--single-arch", action="store_true")
+
+    args = parser.parse_args(namespace=BuildLayerNamespace())
+
+    setup_build_environment(args)
+
+    if not args.skip_download:
+        docker_download_wheels_from_matrix(args)
+
+    if not args.skip_install:
+        docker_install_wheels_from_matrix(args)
