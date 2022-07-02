eb1443426dbc9b32e2dc0f60387c853dc488aafa
Shane Spencer <305301+whardier@users.noreply.github.com>
2022-07-02 18:49:05 +0000

    rework the instrumentation span to be more compatible with current console processing

--- python/packages/aws-lambda-otel-extension/src/serverless_aws_lambda_otel_extension_wrapper/__init__.py
+++ python/packages/aws-lambda-otel-extension/src/serverless_aws_lambda_otel_extension_wrapper/__init__.py
@@ -4,9 +4,9 @@
 #
 #   src/serverless (doesn't have __init__.py)
 #   src/serverless/aws_lambda_otel_extension (has __init__.py)
-#   src/serverless_aws_lambda_otel_extension_wrapper_handler (has __init__.py)
+#   src/serverless_aws_lambda_otel_extension_wrapper (has __init__.py)
 #
 # We want to retain the use of the `serverless.*` namespace for practicality reasons and wedges like this are required
 # when dealing with code that operates similar to the AWS bootstrap code via older import discovery methods.
 
-from serverless.aws_lambda_otel_extension.wrapper.handler import auto_instrumenting_handler
+from serverless.aws_lambda_otel_extension.wrapper.handler import auto_instrumenting_handler  # noqa
