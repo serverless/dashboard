b2bb89dd919eee73e9d72c795828db16d16c71f5
Mariusz Nowak <medyk@medikoo.com>
2022-07-04 17:21:42 +0200

    refactor: Centralize user settings handling

--- node/packages/aws-lambda-otel-extension/external/otel-extension-external/user-settings.js
+++ node/packages/aws-lambda-otel-extension/external/otel-extension-external/user-settings.js
@@ -1,41 +1,5 @@
-// Ensure its same as internal/otel-extension-internal-node/user-settings.js
-// TODO: Possibly centralize this logic assuming convinncing solution is found
+// This module is overriden with content of ../../lib/user-settings
 
 'use strict';
 
-module.exports = {
-  common: { destination: {} },
-  logs: {},
-  metrics: { outputType: 'protobuf' },
-  request: {},
-  response: {},
-  traces: { outputType: 'protobuf' },
-};
-
-const userSettingsText = process.env.SLS_OTEL_USER_SETTINGS;
-
-if (!userSettingsText) return;
-
-const userSettings = (() => {
-  try {
-    return JSON.parse(userSettingsText);
-  } catch (error) {
-    process._rawDebug(`Resolution of user settings failed with: ${error.message}`);
-    return null;
-  }
-})();
-
-if (!userSettings) return;
-
-// Normalize
-if (userSettings.common) {
-  if (userSettings.common.destination) {
-    Object.assign(module.exports.common.destination, userSettings.common.destination);
-  }
-}
-
-for (const subSettingsName of ['logs', 'metrics', 'request', 'response', 'traces']) {
-  if (userSettings[subSettingsName]) {
-    Object.assign(module.exports[subSettingsName], userSettings[subSettingsName]);
-  }
-}
+module.exports = require('../../lib/user-settings');
