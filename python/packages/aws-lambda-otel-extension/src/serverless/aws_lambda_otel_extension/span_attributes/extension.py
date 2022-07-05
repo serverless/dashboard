03872a91d94039411530feff44468e0217345d98
Shane Spencer <305301+whardier@users.noreply.github.com>
2022-07-05 22:26:25 +0000

    rename internal attribute values

--- python/packages/aws-lambda-otel-extension/src/serverless/aws_lambda_otel_extension/span_attributes/extension.py
+++ python/packages/aws-lambda-otel-extension/src/serverless/aws_lambda_otel_extension/span_attributes/extension.py
@@ -1,19 +1,21 @@
 class SlsExtensionSpanAttributes:
 
-    SLS_EVENT_TYPE = "sls.event.type"
+    SLS_EVENT_TYPE = "sls.event_type"
 
-    SLS_HANDLER_ACTUAL = "sls.handler.actual"
-    SLS_HANDLER_ENTRYPOINT = "sls.handler.entrypoint"
-    SLS_HANDLER_MIDDLEWARE = "sls.handler.middleware"
-    SLS_HANDLER_NAME = "sls.handler.name"
-    SLS_HANDLER_RENAMED = "sls.handler.renamed"
-    SLS_HANDLER_REQUEST_JSON = "sls.handler.request.json"
-    SLS_HANDLER_RESPONSE_JSON = "sls.handler.response.json"
-    SLS_HANDLER_REQUEST_REPRESENTATION = "sls.handler.request.representation"
-    SLS_HANDLER_REQUEST_TRUNCATED = "sls.handler.request.truncated"
-    SLS_HANDLER_RESPONSE_REPRESENTATION = "sls.handler.response.representation"
-    SLS_HANDLER_RESPONSE_TRUNCATED = "sls.handler.response.truncated"
+    SLS_HANDLER_ACTUAL = "sls.handler_actual"
+    SLS_HANDLER_ENTRYPOINT = "sls.handler_entrypoint"
+    SLS_HANDLER_MIDDLEWARE = "sls.handler_middleware"
+    SLS_HANDLER_NAME = "sls.handler_name"
+    SLS_HANDLER_RENAMED = "sls.handler_renamed"
 
-    SLS_SPAN_TYPE = "sls.span.type"
+    SLS_HANDLER_REQUEST_JSON = "sls.handler_request_json"
+    SLS_HANDLER_REQUEST_REPRESENTATION = "sls.handler_request_representation"
+    SLS_HANDLER_REQUEST_TRUNCATED = "sls.handler_request_truncated"
+
+    SLS_HANDLER_RESPONSE_JSON = "sls.handler_response_json"
+    SLS_HANDLER_RESPONSE_REPRESENTATION = "sls.handler_response_representation"
+    SLS_HANDLER_RESPONSE_TRUNCATED = "sls.handler_response_truncated"
+
+    SLS_SPAN_TYPE = "sls.span_type"
 
     SLS_ORIGINAL_PROPERTIES = "sls.original_properties"
