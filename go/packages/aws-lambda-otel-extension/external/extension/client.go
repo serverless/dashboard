ae0b666941d5284dd28a3d61631925ec1f51bf86
Roberto Faga <robertofaga@gmail.com>
2022-07-08 17:58:26 -0300

    Remove unused code for extension

--- go/packages/aws-lambda-otel-extension/external/extension/client.go
+++ go/packages/aws-lambda-otel-extension/external/extension/client.go
@@ -12,13 +12,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// AWS /register event
-type RegisterResponse struct {
-	FunctionName    string `json:"functionName"`
-	FunctionVersion string `json:"functionVersion"`
-	Handler         string `json:"handler"`
-}
-
 // AWS /event/next event
 type Tracing struct {
 	Type  string `json:"type"`
@@ -34,11 +27,6 @@ type NextEventResponse struct {
 
 // EventType is the event type from /event/next event
 type EventType string
-
-//  Response for /init/error and /exit/error
-type StatusResponse struct {
-	Status string `json:"status"`
-}
 
 const (
 	Invoke        EventType = "INVOKE"
@@ -94,18 +82,9 @@ func (e *Client) Register(ctx context.Context) error {
 		return fmt.Errorf("request failed with status %s", httpRes.Status)
 	}
 	defer httpRes.Body.Close()
-	body, err := ioutil.ReadAll(httpRes.Body)
-	if err != nil {
-		return err
-	}
-	res := RegisterResponse{}
-	err = json.Unmarshal(body, &res)
-	if err != nil {
-		return err
-	}
 	e.ExtensionID = httpRes.Header.Get(extensionIdentiferHeader)
 
-	e.logger.Debug("Registered extension", zap.String("extensionID", e.ExtensionID), zap.String("response", lib.PrettyPrint(res)))
+	e.logger.Debug("Registered extension", zap.String("extensionID", e.ExtensionID))
 
 	return nil
 }
@@ -132,66 +111,6 @@ func (e *Client) NextEvent(ctx context.Context) (*NextEventResponse, error) {
 		return nil, err
 	}
 	res := NextEventResponse{}
-	err = json.Unmarshal(body, &res)
-	if err != nil {
-		return nil, err
-	}
-	return &res, nil
-}
-
-// InitError reports an initialization error to the platform. Call it when you registered but failed to initialize
-func (e *Client) InitError(ctx context.Context, errorType string) (*StatusResponse, error) {
-	url := e.baseURL + "/init/error"
-
-	httpReq, err := http.NewRequestWithContext(ctx, "POST", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	httpReq.Header.Set(extensionIdentiferHeader, e.ExtensionID)
-	httpReq.Header.Set(extensionErrorType, errorType)
-	httpRes, err := e.httpClient.Do(httpReq)
-	if err != nil {
-		return nil, err
-	}
-	if httpRes.StatusCode != 200 {
-		return nil, fmt.Errorf("request failed with status %s", httpRes.Status)
-	}
-	defer httpRes.Body.Close()
-	body, err := ioutil.ReadAll(httpRes.Body)
-	if err != nil {
-		return nil, err
-	}
-	res := StatusResponse{}
-	err = json.Unmarshal(body, &res)
-	if err != nil {
-		return nil, err
-	}
-	return &res, nil
-}
-
-// ExitError reports an error to the platform before exiting. Call it when you encounter an unexpected failure
-func (e *Client) ExitError(ctx context.Context, errorType string) (*StatusResponse, error) {
-	url := e.baseURL + "/exit/error"
-
-	httpReq, err := http.NewRequestWithContext(ctx, "POST", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	httpReq.Header.Set(extensionIdentiferHeader, e.ExtensionID)
-	httpReq.Header.Set(extensionErrorType, errorType)
-	httpRes, err := e.httpClient.Do(httpReq)
-	if err != nil {
-		return nil, err
-	}
-	if httpRes.StatusCode != 200 {
-		return nil, fmt.Errorf("request failed with status %s", httpRes.Status)
-	}
-	defer httpRes.Body.Close()
-	body, err := ioutil.ReadAll(httpRes.Body)
-	if err != nil {
-		return nil, err
-	}
-	res := StatusResponse{}
 	err = json.Unmarshal(body, &res)
 	if err != nil {
 		return nil, err
--- go/packages/aws-lambda-otel-extension/external/go.mod
+++ go/packages/aws-lambda-otel-extension/external/go.mod
@@ -3,6 +3,8 @@ module aws-lambda-otel-extension/external
 go 1.18
 
 require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/valyala/fasthttp v1.38.0
 	go.uber.org/zap v1.21.0
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
 	google.golang.org/protobuf v1.28.0
@@ -10,10 +12,8 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/klauspost/compress v1.15.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.38.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
