ee1b7db28c65d193cc06300d1698e68344c3ef1f
Max Marze <max@marze.dev>
2022-07-27 21:56:28 -0400

    Setup Rollup for building

---
+++ proto/rollup.config.js
@@ -0,0 +1,46 @@
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+import multi from "@rollup/plugin-multi-entry";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: [
+      "out/platformcore/proto/common/v1/common.pb.ts",
+      "out/platformcore/proto/instrumentation/v1/collector.pb.ts",
+      "out/platformcore/proto/instrumentation/v1/log.pb.ts",
+      "out/platformcore/proto/instrumentation/v1/metric.pb.ts",
+      "out/platformcore/proto/instrumentation/v1/trace.pb.ts",
+    ],
+    // input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript(), multi()],
+  },
+  {
+    input: [
+      "dist/dts/common/v1/common.pb.d.ts",
+      "dist/dts/instrumentation/v1/collector.pb.d.ts",
+      "dist/dts/instrumentation/v1/log.pb.d.ts",
+      "dist/dts/instrumentation/v1/metric.pb.d.ts",
+      "dist/dts/instrumentation/v1/trace.pb.d.ts",
+    ],
+    output: [
+      {
+        file: pkg.types,
+        format: "es",
+      },
+    ],
+    plugins: [multi(), dts()],
+  },
+];
