d91c10c5621a285d848bdc3b4c58fa814afd5b9f
Mariusz Nowak <medyk@medikoo.com>
2022-08-23 13:23:57 +0200

    feat: `aws.lambda.arch` tag configuration

--- node/packages/aws-lambda-sdk/trace-spans/aws-lambda.js
+++ node/packages/aws-lambda-sdk/trace-spans/aws-lambda.js
@@ -2,7 +2,22 @@
 
 const TraceSpan = require('../lib/trace-span');
 
-module.exports = new TraceSpan('aws.lambda', {
+const awsLambdaSpan = new TraceSpan('aws.lambda', {
   startTime: EvalError.$serverlessAwsLambdaInitializationStartTime,
   immediateDescendants: ['aws.lambda.initialization'],
 });
+
+const arch = (() => {
+  switch (process.arch) {
+    case 'x64':
+      return 'x86_64';
+    case 'arm64':
+      return 'arm64';
+    default:
+      process.stderr.write(`Serverless SDK Error: Unrecognized architecture: "${process.arch}"\n`);
+      return null;
+  }
+})();
+if (arch) awsLambdaSpan.tags.set('aws.lambda.arch', arch);
+
+module.exports = awsLambdaSpan;
