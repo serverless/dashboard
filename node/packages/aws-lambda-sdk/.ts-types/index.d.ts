887bbc4af82662d37f7fea8c8303f773f5a52506
Mariusz Nowak <medyk@medikoo.com>
2022-09-15 16:19:25 +0200

    refactor: Make `serverlessSdk._initialize` private
    It's not intended to be invoke manually by users

--- node/packages/aws-lambda-sdk/.ts-types/index.d.ts
+++ node/packages/aws-lambda-sdk/.ts-types/index.d.ts
@@ -1,12 +1,5 @@
 import TraceSpan from './lib/trace-span';
 
-export interface SdkOptions {
-  orgId?: string;
-  disableHttpMonitoring?: boolean;
-  disableRequestMonitoring?: boolean;
-  disableResponseMonitoring?: boolean;
-}
-
 interface traceSpans {
   awsLambda: TraceSpan;
   awsLambdaInitialization: TraceSpan;
@@ -15,7 +8,6 @@ interface traceSpans {
 interface Sdk {
   orgId: string;
   traceSpans: traceSpans;
-  initialize(options?: SdkOptions): Sdk;
 }
 
 export default Sdk;
