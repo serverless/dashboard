a29d58b5d41febe7608b0bee22a7a22e3755da79
Mariusz Nowak <medyk@medikoo.com>
2022-09-20 15:55:22 +0200

    test: Improve normalization of proto objects

--- node/packages/aws-lambda-sdk/test/utils/normalize-proto-object.js
+++ node/packages/aws-lambda-sdk/test/utils/normalize-proto-object.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const { isLong } = require('long');
+
 module.exports = (obj) => {
   const entries = Array.isArray(obj) ? obj.entries() : Object.entries(obj);
   for (const [key, value] of entries) {
-    if (value == null) delete obj[key];
-    else if (Array.isArray(value)) module.exports(value);
-    else if (typeof value === 'object') module.exports(value);
+    if (value == null) {
+      delete obj[key];
+    } else if (Array.isArray(value)) {
+      module.exports(value);
+    } else if (isLong(value)) {
+      obj[key] = Number(String(value));
+    } else if (typeof value === 'object') {
+      module.exports(value);
+    }
   }
   return obj;
 };
