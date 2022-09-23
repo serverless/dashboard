ce2f6e4132f082e967771e33d7decd743079ce94
Mariusz Nowak <medyk@medikoo.com>
2022-09-23 15:57:46 +0200

    perf: Ensure to not add unused dependencies to build

--- node/packages/aws-lambda-sdk/scripts/lib/build.js
+++ node/packages/aws-lambda-sdk/scripts/lib/build.js
@@ -39,6 +39,7 @@ module.exports = async (distFilename) => {
             '--bundle',
             '--platform=node',
             '--external:./',
+            '--external:../',
           ])
         ).stdoutBuffer
       );
