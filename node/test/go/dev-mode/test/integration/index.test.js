c529d9cfc482c816ac4d8697d99ee51a9d1181c9
Dan Jarvis <danwakeem@gmail.com>
2022-10-04 08:14:36 -0500

    chore: Removed unused code

--- node/test/go/dev-mode/test/integration/index.test.js
+++ node/test/go/dev-mode/test/integration/index.test.js
@@ -61,16 +61,6 @@ describe('Integration', function () {
             const allReqResData = invocationsData.map((data) => data.reqRes);
             expect(allReqResData[0].length).to.equal(0);
             expect(allReqResData[1].length).to.equal(0);
-
-            // for (const [, reqRes] of invocationsData.map((data) => data.reqRes).entries()) {
-            //   expect(reqRes.length).to.equal(1);
-            //   for (const payload of reqRes) {
-            //     expect(payload).to.haveOwnProperty('region');
-            //     expect(payload).to.haveOwnProperty('accountId');
-            //     expect(payload).to.haveOwnProperty('timestamps');
-            //     expect(payload).to.haveOwnProperty('payloads');
-            //   }
-            // }
           },
         },
       },
