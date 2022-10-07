8dec4e03011cb4892248aa57a3625bef0e4ca4f6
Mariusz Nowak <medyk@medikoo.com>
2022-10-07 14:29:28 +0200

    test: Upgrade performance test to confirm on external communication

--- node/packages/aws-lambda-sdk/test/benchmark/performance.test.js
+++ node/packages/aws-lambda-sdk/test/benchmark/performance.test.js
@@ -12,22 +12,29 @@ describe('performance', function () {
   let results;
   before(async () => {
     const resultsMap = await runBenchmarks({
-      benchmarkVariants: new Set(['internal']),
+      benchmarkVariants: new Set(['internal', 'internalAndExternal']),
       useCases: new Set(['callback']),
     });
-    ({ results } = resultsMap.get('callback').get('internal'));
+    results = resultsMap.get('callback');
   });
 
   // TODO: Reduce acceptable durations once improvements are made
   it('should introduce reasonable initialization overhead', () => {
-    expect(results.initialization.total.median).to.be.below(200);
+    expect(results.get('internal').results.initialization.total.median).to.be.below(250);
+    expect(results.get('internalAndExternal').results.initialization.total.median).to.be.below(300);
   });
 
   it('should introduce reasonable first invocation overhead', () => {
-    expect(results.invocation.first.total.median).to.be.below(10);
+    expect(results.get('internal').results.invocation.first.total.median).to.be.below(10);
+    expect(results.get('internalAndExternal').results.invocation.first.total.median).to.be.below(
+      35
+    );
   });
 
   it('should introduce reasonable following invocation overhead', () => {
-    expect(results.invocation.following.total.median).to.be.below(10);
+    expect(results.get('internal').results.invocation.following.total.median).to.be.below(10);
+    expect(results.get('internalAndExternal').results.invocation.first.total.median).to.be.below(
+      35
+    );
   });
 });
