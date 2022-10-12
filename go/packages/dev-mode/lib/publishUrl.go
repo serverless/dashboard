8450bf3f1fa6d03d99abb30014a8445fc5425fcd
Dan Jarvis <danwakeem@gmail.com>
2022-10-12 14:44:54 -0500

    chore: Added a few more exceptions to region map

--- go/packages/dev-mode/lib/publishUrl.go
+++ go/packages/dev-mode/lib/publishUrl.go
@@ -10,6 +10,10 @@ func GetBaseUrl() string {
 	// using the region closest to them in that case
 	regionMap := map[string]string{
 		"ap-northeast-3": "ap-northeast-2",
+		"ap-southeast-3": "ap-southeast-2",
+		"me-central-1":   "me-south-1",
+		"us-gov-east-1":  "us-east-1",
+		"us-gov-west-1":  "us-west-1",
 	}
 
 	region := os.Getenv("AWS_REGION")
