c3e5b675dcad520e906bf9911692036d7ba3af1b
Mariusz Nowak <medyk@medikoo.com>
2022-10-31 19:36:04 +0100

    test: Relax performance restrictions

--- node/packages/aws-lambda-sdk/test/benchmark/performance.test.js
+++ node/packages/aws-lambda-sdk/test/benchmark/performance.test.js
@@ -27,14 +27,14 @@ describe('performance', function () {
   it('should introduce reasonable first invocation overhead', () => {
     expect(results.get('internal').results.invocation.first.total.median).to.be.below(11);
     expect(results.get('internalAndExternal').results.invocation.first.total.median).to.be.below(
-      35
+      40
     );
   });
 
   it('should introduce reasonable following invocation overhead', () => {
     expect(results.get('internal').results.invocation.following.total.median).to.be.below(10);
     expect(results.get('internalAndExternal').results.invocation.first.total.median).to.be.below(
-      35
+      40
     );
   });
 });
