75ac2406f17cf9ad670c49cecf66d403e83b327e
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-12-28 03:08:18 -0500

    Implement `serverlessSdk` and its methods

--- python/packages/sdk/sdk/__init__.py
+++ python/packages/sdk/sdk/__init__.py
@@ -1,5 +1,39 @@
-from typing import Final
+from typing_extensions import Final
+from typing import List, Optional
+from dataclasses import dataclass
+from os import environ
+
 from pkg_resources import get_distribution
 
 
-__version__: Final[str]
+# public exports
+__all__: Final[List[str]] = [
+  'serverlessSdk',
+]
+
+__version__: Final[str] = ...
+__name__: Final[str] = ...
+
+
+SLS_ORG_ID: Final[str] = 'SLS_ORG_ID'
+
+
+@dataclass
+class Options:
+  orgId: Optional[str] = None
+
+
+class _ServerlessSdk:
+  name: Final[str] = __name__
+  version: Final[str] = __version__
+
+  traceSpans: Final = ...
+  instrumentation: Final = ...
+
+  orgId: Optional[str] = None
+
+  def _initialize(self, options: Options = Options()):
+    self.orgId = environ[SLS_ORG_ID] or options.orgId
+
+
+serverlessSdk: Final[_ServerlessSdk] = _ServerlessSdk()
