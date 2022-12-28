cb1575bfff5741a93caba10d199b7ef9446f0b39
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-12-28 04:20:09 -0500

    Add `pytest` fixture and tests

--- python/packages/sdk/tests/test_sdk.py
+++ python/packages/sdk/tests/test_sdk.py
@@ -1,3 +1,18 @@
+from typing import TypeAlias
+
+import pytest
+
+
+ServerlessSdk: TypeAlias = 'ServerlessSdk'
+
+
+@pytest.fixture
+def sdk() -> ServerlessSdk:
+  from ..sdk import serverlessSdk
+
+  return serverlessSdk
+
+
 def test_can_import_serverless_sdk():
   try:
     from ..sdk import serverlessSdk
@@ -6,3 +21,11 @@ def test_can_import_serverless_sdk():
     raise AssertionError("Cannot import `serverlessSdk`")
 
 
+def test_has_name(sdk: ServerlessSdk):
+  assert hasattr(sdk, 'name')
+  assert isinstance(sdk.name, str)
+
+
+def test_has_version(sdk: ServerlessSdk):
+  assert hasattr(sdk, 'version')
+  assert isinstance(sdk.version, str)
