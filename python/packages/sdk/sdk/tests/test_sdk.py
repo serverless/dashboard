5445d864976811f59d299b56920313ca54e4ef07
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-12-28 22:51:21 -0500

    Migrate code to `black` formatting

--- python/packages/sdk/sdk/tests/test_sdk.py
+++ python/packages/sdk/sdk/tests/test_sdk.py
@@ -5,74 +5,74 @@ import inspect
 import pytest
 
 
-ServerlessSdk: TypeAlias = 'ServerlessSdk'
+ServerlessSdk: TypeAlias = "ServerlessSdk"
 
 
 @pytest.fixture
 def sdk() -> ServerlessSdk:
-  from .. import serverlessSdk
+    from .. import serverlessSdk
 
-  return serverlessSdk
+    return serverlessSdk
 
 
 def test_can_import_serverless_sdk():
-  try:
-    from .. import serverlessSdk
+    try:
+        from .. import serverlessSdk
 
-  except ImportError:
-    raise AssertionError("Cannot import `serverlessSdk`")
+    except ImportError:
+        raise AssertionError("Cannot import `serverlessSdk`")
 
 
 def test_has_name(sdk: ServerlessSdk):
-  assert hasattr(sdk, 'name')
-  assert isinstance(sdk.name, str)
+    assert hasattr(sdk, "name")
+    assert isinstance(sdk.name, str)
 
 
 def test_has_version(sdk: ServerlessSdk):
-  assert hasattr(sdk, 'version')
-  assert isinstance(sdk.version, str)
+    assert hasattr(sdk, "version")
+    assert isinstance(sdk.version, str)
 
 
 def test_has_tracespans(sdk: ServerlessSdk):
-  assert hasattr(sdk, 'traceSpans')
+    assert hasattr(sdk, "traceSpans")
 
 
 def test_has_instrumentation(sdk: ServerlessSdk):
-  assert hasattr(sdk, 'instrumentation')
+    assert hasattr(sdk, "instrumentation")
 
 
 def test_has_initialize_method_with_params(sdk: ServerlessSdk):
-  assert hasattr(sdk, '_initialize')
-  assert isinstance(sdk._initialize, MethodType)
+    assert hasattr(sdk, "_initialize")
+    assert isinstance(sdk._initialize, MethodType)
 
-  # check if method takes `options` param
-  signature = inspect.signature(sdk._initialize)
-  params = signature.parameters
+    # check if method takes `options` param
+    signature = inspect.signature(sdk._initialize)
+    params = signature.parameters
 
-  assert len(params) >= 1
-  assert 'options' in params
+    assert len(params) >= 1
+    assert "options" in params
 
 
 def test_initialize_supports_options(sdk: ServerlessSdk):
-  from .. import Options
+    from .. import Options
 
-  org_id = 'test'
-  options = Options(orgId=org_id)
-  assert options.orgId == org_id
+    org_id = "test"
+    options = Options(orgId=org_id)
+    assert options.orgId == org_id
 
-  sdk._initialize(options)
-  assert sdk.orgId == options.orgId
+    sdk._initialize(options)
+    assert sdk.orgId == options.orgId
 
 
 def test_initialize_favors_env_var(sdk: ServerlessSdk):
-  from os import environ
-  from .. import Options
+    from os import environ
+    from .. import Options
 
-  options = Options(orgId='opts')
+    options = Options(orgId="opts")
 
-  env: str = 'env'
-  environ['SLS_ORG_ID'] = env
+    env: str = "env"
+    environ["SLS_ORG_ID"] = env
 
-  sdk._initialize(options)
-  assert sdk.orgId != options.orgId
-  assert sdk.orgId == env
+    sdk._initialize(options)
+    assert sdk.orgId != options.orgId
+    assert sdk.orgId == env
