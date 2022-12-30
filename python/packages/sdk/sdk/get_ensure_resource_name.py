9d51214d85c0443d245b284d88627ad26b9b5011
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-12-30 02:45:14 -0500

    Use raw strings

--- python/packages/sdk/sdk/get_ensure_resource_name.py
+++ python/packages/sdk/sdk/get_ensure_resource_name.py
@@ -6,7 +6,7 @@ from js_regex import compile
 
 # from https://github.com/serverless/console/blob/main/node/packages/sdk/lib/get-ensure-resource-name.js#L7
 RE: Final[str] = \
-    "^[a-z][a-z0-9]*(?:_[a-z][a-z0-9]*)*(?:\.[a-z][a-z0-9]*(?:_[a-z][a-z0-9]*)*)*$"
+    r"^[a-z][a-z0-9]*(?:_[a-z][a-z0-9]*)*(?:\.[a-z][a-z0-9]*(?:_[a-z][a-z0-9]*)*)*$"
 
 RE_C: Final[Pattern[str]] = compile(RE)
 
