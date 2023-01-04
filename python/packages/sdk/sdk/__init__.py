c77aaba230b9012cbdb22c66ebee9f462f3a094f
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-03 23:18:50 -0500

    Remove unused imports

--- python/packages/sdk/sdk/__init__.py
+++ python/packages/sdk/sdk/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from typing_extensions import Final
 from typing import Dict, List, Optional
-from dataclasses import dataclass
 from os import environ
 
 from importlib_metadata import version, packages_distributions
