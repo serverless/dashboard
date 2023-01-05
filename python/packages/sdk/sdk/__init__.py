0e2290b22e2d6b3784e25d82240e3c1e270e8703
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-04 23:08:52 -0500

    Move metadata to `base`, only use top-level module for exports

--- python/packages/sdk/sdk/__init__.py
+++ python/packages/sdk/sdk/__init__.py
@@ -1,50 +1,15 @@
 from __future__ import annotations
 
 from typing_extensions import Final
-from typing import Dict, List, Optional
-from dataclasses import dataclass
-from os import environ
+from typing import List
 
-from importlib_metadata import version, packages_distributions  # available in Python >=3.8
+from .sdk import ServerlessSdk
+from .trace_span import TraceSpan
 
 
 # public exports
 __all__: Final[List[str]] = [
     "serverlessSdk",
-    "Options",
 ]
-
-FIRST: Final[int] = 0
-
-_packages: Final[Dict[str, List[str]]] = packages_distributions()
-_pkg_name: Final[str] = __name__ or __package__
-_distribution: Final[List[str]] = _packages[_pkg_name]
-
-__name__: Final[str] = _distribution[FIRST]
-__version__: Final[str] = version(__name__)
-
-SLS_ORG_ID: Final[str] = "SLS_ORG_ID"
-
-
-@dataclass
-class Options:
-    orgId: Optional[str] = None
-
-
-class ServerlessSdk:
-    name: Final[str] = __name__
-    version: Final[str] = __version__
-
-    traceSpans: Final = ...
-    instrumentation: Final = ...
-
-    orgId: Optional[str] = None
-
-    def _initialize(self, options: Options = Options()):
-        self.orgId = environ.get(SLS_ORG_ID, default=options.orgId)
-
-    def createTraceSpan(self, name: str, **options):
-        pass
-
 
 serverlessSdk: Final[ServerlessSdk] = ServerlessSdk()
--- python/packages/sdk/sdk/base.py
+++ python/packages/sdk/sdk/base.py
@@ -1,4 +1,21 @@
-from typing import List, Union
+from __future__ import annotations
+
+from typing import Dict, List, Union
+
+from importlib_metadata import packages_distributions, version
+from typing_extensions import Final
+
+
+FIRST: Final[int] = 0
+SLS_ORG_ID: Final[str] = "SLS_ORG_ID"
+
+_packages: Final[Dict[str, List[str]]] = packages_distributions()
+_pkg_name: Final[str] = __name__ or __package__
+_distribution: Final[List[str]] = _packages[_pkg_name]
+
+# module metadata
+__name__: Final[str] = _distribution[FIRST]
+__version__: Final[str] = version(__name__)
 
 
 TraceId = str
