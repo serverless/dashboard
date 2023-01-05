c5bd42939a3b905d8016b0c1856b35760e9e9460
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-04 23:08:52 -0500

    Move metadata to `base`, only use top-level module for exports

--- python/packages/sdk/sdk/__init__.py
+++ python/packages/sdk/sdk/__init__.py
@@ -1,43 +1,15 @@
 from __future__ import annotations
-from typing_extensions import Final
-from typing import Dict, List, Optional
-from os import environ
 
-from importlib_metadata import version, packages_distributions
+from typing_extensions import Final
+from typing import List
+
+from .sdk import ServerlessSdk
+from .trace_span import TraceSpan
 
 
 # public exports
 __all__: Final[List[str]] = [
     "serverlessSdk",
 ]
-
-FIRST: Final[int] = 0
-
-_packages: Final[Dict[str, List[str]]] = packages_distributions()
-_pkg_name: Final[str] = __name__ or __package__
-_distribution: Final[List[str]] = _packages[_pkg_name]
-
-__name__: Final[str] = _distribution[FIRST]
-__version__: Final[str] = version(__name__)
-
-
-SLS_ORG_ID: Final[str] = "SLS_ORG_ID"
-
-
-class ServerlessSdk:
-    name: Final[str] = __name__
-    version: Final[str] = __version__
-
-    traceSpans: Final = ...
-    instrumentation: Final = ...
-
-    orgId: Optional[str] = None
-
-    def _initialize(self, org_id: Optional[str] = None):
-        self.orgId = environ.get(SLS_ORG_ID, default=org_id)
-
-    def createTraceSpan(self, name: str, **options):
-        pass
-
 
 serverlessSdk: Final[ServerlessSdk] = ServerlessSdk()
