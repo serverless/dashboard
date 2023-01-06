95bfed99fd33096ced5cff63f1241819982d4d77
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-06 16:18:40 -0500

    Test `generate_id()` and `get_ensure_resource_name()`

---
+++ python/packages/sdk/sdk/tests/test_get_ensure_resource_name.py
@@ -0,0 +1,27 @@
+from typing_extensions import Final
+import pytest
+
+from ..get_ensure_resource_name import get_ensure_resource_name, is_valid_name
+
+
+VALID_NAME: Final[str] = 'This.is.a._valid.name123'
+INVALID_NAME: Final[str] = "This isn't a valid name"
+
+
+def test_is_valid_name():
+    assert is_valid_name(VALID_NAME)
+    assert not is_valid_name(INVALID_NAME)
+
+
+def test_get_ensure_resource_name():
+    assert get_ensure_resource_name(VALID_NAME) == VALID_NAME
+
+    with pytest.raises(ValueError):
+        get_ensure_resource_name(INVALID_NAME)
+
+    with pytest.raises(TypeError):
+        get_ensure_resource_name(None)
+
+    with pytest.raises(TypeError):
+        as_bytes: bytes = VALID_NAME.encode()
+        get_ensure_resource_name(as_bytes)
