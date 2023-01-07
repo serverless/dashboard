03237c3cb8cede7e4c06c90da5f631a3ec6c53eb
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-06 23:50:15 -0500

    Add more tests and cases against `Tags`

--- python/packages/sdk/sdk/tests/test_tags.py
+++ python/packages/sdk/sdk/tests/test_tags.py
@@ -1,17 +1,22 @@
 from math import inf, nan
-from typing import Tuple
+from typing import Tuple, Any
 
 import pytest
-from typing_extensions import Any, Final
+from typing_extensions import Final
 
 from ..base import ValidTags
-from ..exceptions import InvalidTraceSpanTagName, InvalidTraceSpanTagValue
-from ..span.tags import ensure_tag_name, ensure_tag_value
+from ..exceptions import (
+    DuplicateTraceSpanName,
+    InvalidTraceSpanTagName,
+    InvalidTraceSpanTagValue,
+)
+from ..span.tags import ensure_tag_name, ensure_tag_value, Tags
 
 
 VALID_NAMES: Final[Tuple[str, ...]] = (
     "validname",
     "a_b_c.d_e_f",
+    "a_b_c1.d_e_f2",
 )
 
 INVALID_NAMES: Final[Tuple[str, ...]] = (
@@ -20,6 +25,7 @@ INVALID_NAMES: Final[Tuple[str, ...]] = (
     "_invalid.name",
     "Invalid.name",
     "inValid.name",
+    "invalid.Name",
 )
 
 
@@ -53,6 +59,11 @@ INVALID_VALUES: Final[Tuple[Any, ...]] = (
 ATTR: Final[str] = "attr"
 
 
+@pytest.fixture
+def tags() -> Tags:
+    return Tags()
+
+
 def test_ensure_tag_name():
     for name in VALID_NAMES:
         assert ensure_tag_name(ATTR, name)
@@ -69,3 +80,38 @@ def test_ensure_tag_value():
     for value in INVALID_VALUES:
         with pytest.raises(InvalidTraceSpanTagValue):
             ensure_tag_value(ATTR, value)
+
+
+def test_tags_valid_names_and_values():
+    for value in VALID_VALUES:
+        tags = Tags()
+
+        for name in VALID_NAMES:
+            tags[name] = value
+
+
+def test_tags_invalid_names_and_values(tags: Tags):
+    for name in INVALID_NAMES:
+        for value in VALID_VALUES:
+            with pytest.raises(InvalidTraceSpanTagName):
+                tags[name] = value
+
+    for name in INVALID_NAMES:
+        for value in INVALID_VALUES:
+            with pytest.raises(InvalidTraceSpanTagName):
+                tags[name] = value
+
+    for name in VALID_NAMES:
+        for value in INVALID_VALUES:
+            with pytest.raises(InvalidTraceSpanTagValue):
+                tags[name] = value
+
+
+def test_tags_duplicate(tags: Tags):
+    for name in VALID_NAMES:
+        tags[name] = "example"
+
+    for name in VALID_NAMES:
+        for value in VALID_VALUES:
+            with pytest.raises(DuplicateTraceSpanName):
+                tags[name] = value
