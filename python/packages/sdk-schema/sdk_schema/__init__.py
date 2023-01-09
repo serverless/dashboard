8c1250329f1f6740fb7d0a34087aa25e8ee3df16
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-08 22:58:37 -0500

    Export `TracePayload` and `RequestResponse`

---
+++ python/packages/sdk-schema/sdk_schema/__init__.py
@@ -0,0 +1,8 @@
+from .serverless.instrumentation.v1.trace_pb2 import TracePayload
+from .serverless.instrumentation.v1.request_response_pb2 import RequestResponse
+
+
+__all__ = [
+    'TracePayload',
+    'RequestResponse',
+]
