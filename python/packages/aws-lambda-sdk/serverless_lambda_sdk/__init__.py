3bb461faf648415ee20fc33f914141a62ffa57da
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-17 16:56:38 -0500

    Add modules

---
+++ python/packages/aws-lambda-sdk/serverless_lambda_sdk/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from serverless_sdk import serverlessSdk
+
+
+__all__ = [
+    "serverlessSdk",
+]
---
+++ python/packages/aws-lambda-sdk/serverless_lambda_sdk/base.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Callable, Tuple, TypeVar
+
+from strenum import StrEnum
+from typing_extensions import Concatenate, Final, ParamSpec, Self
+
+
+NEW_HANDLER: Final[str] = "/opt/sls-sdk-python/wrapper.handler"
+PYTHON_EXTS: Final[Tuple[str, ...]] = (".py", ".pyc", ".pyo", ".pyd")
+
+
+P = ParamSpec("P")
+I_P = ParamSpec("I_P")
+T = TypeVar("T")
+
+HandlerPath = str
+Handler = Callable[P, T]
+InstrumentParams = Concatenate[HandlerPath, I_P]
+Instrument = Callable[InstrumentParams, Handler]
+Loader = Callable[[HandlerPath], Handler]
+
+
+class Env(StrEnum):
+    HANDLER: Self = "_HANDLER"
+    ORIGIN_HANDLER: Self = "_ORIGIN_HANDLER"
+    LAMBDA_RUNTIME_DIR: Self = "LAMBDA_RUNTIME_DIR"
+    LAMBDA_TASK_ROOT: Self = "LAMBDA_TASK_ROOT"
+
+    SLS_ORG_ID: Self = "SLS_ORG_ID"
+    SLS_SDK_DEBUG: Self = "SLS_SDK_DEBUG"
