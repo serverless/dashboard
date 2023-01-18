8663945cc29976bbdfd364ded9bd2bad1e51608b
Dan Jarvis <Danwakeem@users.noreply.github.com>
2023-01-18 15:37:16 -0600

    chore: Update error comment

--- node/test/go/dev-mode/test/fixtures/lambdas/with-internal.js
+++ node/test/go/dev-mode/test/fixtures/lambdas/with-internal.js
@@ -3,6 +3,6 @@
 module.exports.handler = (event, context, callback) => {
   console.log('with-internal 1');
   console.warn('with-internal warning 1');
-  console.error(new Error('with-internal warning 1'));
+  console.error(new Error('with-internal error 1'));
   callback(null, 'ok');
 };
