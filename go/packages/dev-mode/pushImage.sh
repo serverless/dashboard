19e3a139a6992f037ac7a6c9e979b62af0116e76
Dan Jarvis <danwakeem@gmail.com>
2023-01-31 14:51:55 -0600

    chore: Updated dockerfile

--- go/packages/dev-mode/pushImage.sh
+++ go/packages/dev-mode/pushImage.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-eval $(aws ecr get-login-password --profile $AWS_PROFILE --region us-east-1 | sed 's|https://||')
+aws ecr get-login-password --profile $AWS_PROFILE --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
 docker build -t dev-mode-extension .
 
 docker tag dev-mode-extension:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/dev-mode-extension:latest
 
-echo "PUSHING...."
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/dev-mode-extension:latest
 echo "DONE"
