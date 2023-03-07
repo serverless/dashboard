4622b970c713cff650e0f318d3c76de7921e5f69
Mariusz Nowak <medyk@medikoo.com>
2023-03-07 18:08:10 +0100

    fix: FIx error handling in layer publication script

--- node/lib/publish-public-layers.js
+++ node/lib/publish-public-layers.js
@@ -36,12 +36,14 @@ module.exports = async ({ bucketName, layerBasename, version, content, githubTag
         const layersRegistry = JSON.parse(
           await toPromise(
             (
-              await awsRequest(S3, 'getObject', { Bucket: bucketName, Key: registryName })
+              await awsRequest(S3, 'getObject', { Bucket: bucketName, Key: registryName }).catch(
+                (error) => {
+                  if (error.Code === 'AccessDenied') return '{}'; // Registry does not exist yet
+                  throw error;
+                }
+              )
             ).Body
-          ).catch((error) => {
-            if (error.Code === 'AccessDenied') return '{}'; // Registry does not exist yet
-            throw error;
-          })
+          )
         );
         _.merge(layersRegistry, newMeta);
         await awsRequest(S3, 'putObject', {
