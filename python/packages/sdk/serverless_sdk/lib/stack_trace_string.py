71f2cca850b4082be8817b5883e3a5bed892c773
Selcuk Cihan <selcukcihan@gmail.com>
2023-03-13 12:47:01 +0300

    Comment for stacktrace resolution

--- python/packages/sdk/serverless_sdk/lib/stack_trace_string.py
+++ python/packages/sdk/serverless_sdk/lib/stack_trace_string.py
@@ -6,12 +6,16 @@ from typing import Optional, Any
 
 def resolve(error: Optional[Any] = None) -> str:
     if isinstance(error, BaseException):
+        # in case of an actual Exception, stack trace is already set up.
         return "".join(
             traceback.format_exception(
                 etype=type(error), value=error, tb=error.__traceback__
             )
         )
     else:
+        # in case of errors that are not exceptions, return the current stack trace
+        # but exclude the most recent 3 frames to make sure stack trace ends at
+        # customer's code and does not include SDK internal methods.
         depth = len(inspect.stack())
         relevant_frame = sys._getframe(3) if depth > 3 else None
         return "".join(traceback.format_stack(f=relevant_frame))
