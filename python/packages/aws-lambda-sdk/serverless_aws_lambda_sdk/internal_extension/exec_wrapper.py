5fb421d8cb3a5a0b8de60128719492fe97e84cc1
Selcuk Cihan <selcukcihan@gmail.com>
2023-03-16 12:38:35 +0300

    fix wrapper init

--- python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/internal_extension/exec_wrapper.py
+++ python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/internal_extension/exec_wrapper.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 import sys
 import os
+import importlib
+from pathlib import Path
 
 
+# This script is executed by AWS Lambda Runtime as follows:
+# /opt/sls-sdk-python/exec_wrapper.py /var/lang/bin/python3 /var/runtime/bootstrap.py
 def main():
     from base import initialize
 
     initialize()
 
-    _, *args = sys.argv
-    command = " ".join(args)
-    os.system(command)
+    # AWS Lambda Runtime sets "LAMBDA_RUNTIME_DIR" env variable to "/var/runtime"
+    # which is needed in the path for the import to work.
+    sys.path.append(os.environ["LAMBDA_RUNTIME_DIR"])
+
+    module_name = Path(sys.argv[2]).stem
+    bootstrap = importlib.import_module(module_name)
+    bootstrap.main()
 
 
 if __name__ == "__main__":
