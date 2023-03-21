396a69c00241974474db8ac424958a54d4803afd
Selcuk Cihan <selcukcihan@gmail.com>
2023-03-21 19:12:00 +0300

    remove incomplete scheduled span implementation

--- python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/instrument/lib/scheduled_spans.py
+++ python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/instrument/lib/scheduled_spans.py
@@ -1,38 +1,7 @@
-import time
-from typing import Callable
 from .sdk import serverlessSdk
 
 serverlessSdk._deferred_telemetry_requests = []
 
-_pending_spans = []
-_pending_captured_events = []
-_is_scheduled = False
-
 
 def flush():
-    return None
-
-
-def _flush():
     pass
-
-
-def _schedule_eventually():
-    _pending_spans.clear()
-    _pending_captured_events.clear()
-
-
-def _captured_event_handler(captured_event):
-    _pending_captured_events.append(captured_event)
-    _schedule_eventually()
-
-
-def _trace_span_close_handler(trace_span):
-    _pending_spans.append(trace_span)
-    _schedule_eventually()
-
-
-if serverlessSdk._is_dev_mode:
-    flush = _flush  # noqa: F811
-    serverlessSdk._event_emitter.on("captured-event", _captured_event_handler)
-    serverlessSdk._event_emitter.on("trace-span-close", _trace_span_close_handler)
