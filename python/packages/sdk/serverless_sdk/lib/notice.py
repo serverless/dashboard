b6b912c7da594a31a3f6062b7eb94f703b7b84db
Selcuk Cihan <selcukcihan@gmail.com>
2023-03-23 12:53:12 +0300

    Add report_notice method and emit span close event

---
+++ python/packages/sdk/serverless_sdk/lib/notice.py
@@ -0,0 +1,17 @@
+from typing import Optional
+import time
+from .trace import TraceSpan
+from .captured_event import CapturedEvent
+
+
+def report(message: str, code, trace_span: Optional[TraceSpan] = None):
+    return CapturedEvent(
+        "telemetry.notice.generated.v1",
+        timestamp=time.perf_counter_ns(),
+        custom_fingerprint=code,
+        tags={
+            "notice.message": message,
+            "notice.type": 1,
+        },
+        trace_span=trace_span,
+    )
