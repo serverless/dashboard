12985ccd3941e8e5cc8d3f6bf3e8db7885132b55
Selcuk Cihan <selcukcihan@gmail.com>
2023-03-28 19:48:38 +0300

    Remove unnecessary conversion

--- python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/instrument/lib/payload_conversion.py
+++ python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/instrument/lib/payload_conversion.py
@@ -3,14 +3,8 @@ from google.protobuf import json_format
 
 
 def to_trace_payload(payload_dct: dict) -> TracePayload:
-    payload = json_format.ParseDict(payload_dct, TracePayload())
-    return payload
+    return json_format.ParseDict(payload_dct, TracePayload())
 
 
 def to_request_response_payload(payload_dct: dict) -> RequestResponse:
-    payload = json_format.ParseDict(payload_dct, RequestResponse())
-    if payload_dct["spanId"]:
-        payload.span_id = str.encode(payload_dct["spanId"])
-    if payload_dct["traceId"]:
-        payload.trace_id = str.encode(payload_dct["traceId"])
-    return payload
+    return json_format.ParseDict(payload_dct, RequestResponse())
