9557b159223b523334380260be7c5e3bee9b6076
Selçuk Cihan <selcukcihan@gmail.com>
2023-04-03 12:40:41 +0300

    fix: Do not initialize user lambda before the runtime (#595)

--- python/packages/aws-lambda-sdk/tests/internal_extension/test_internal_extension_exec_wrapper.py
+++ python/packages/aws-lambda-sdk/tests/internal_extension/test_internal_extension_exec_wrapper.py
@@ -1,49 +1,149 @@
+import pytest
 import sys
 from pathlib import Path
 import os
+from unittest.mock import MagicMock
+import importlib
 
 
-def test_exec_wrapper_succeeds(monkeypatch):
-    # given
-    env = dict(os.environ)
-    monkeypatch.setattr(os, "environ", env)
+@pytest.fixture()
+def exec_wrapper_main():
+    import serverless_aws_lambda_sdk.internal_extension.exec_wrapper
 
-    os.environ["_TEST_INTERNAL_EXTENSION"] = "0"
+    importlib.reload(serverless_aws_lambda_sdk.internal_extension.exec_wrapper)
+    yield serverless_aws_lambda_sdk.internal_extension.exec_wrapper.main
 
-    monkeypatch.setenv("_HANDLER", "fixtures.lambdas.success.handler")
+
+@pytest.fixture()
+def wrapper_patch(monkeypatch):
     monkeypatch.setenv("LAMBDA_TASK_ROOT", str(Path(__file__).parent.resolve()))
     monkeypatch.setenv("SLS_ORG_ID", "test-org")
     monkeypatch.setenv("SLS_SDK_DEBUG", "1")
     monkeypatch.setenv("LAMBDA_RUNTIME_DIR", "/var/runtime")
-    monkeypatch.setattr(
-        "sys.argv",
-        [
-            "/opt/sls-sdk-python/exec_wrapper.py",
-            "/var/lang/bin/python3",
-            str(Path(__file__).resolve()),
-        ],
-    )
-    sys.path.append(
-        str(
-            (
-                Path(__file__).parent.parent.parent
-                / "serverless_aws_lambda_sdk/internal_extension"
-            ).resolve()
-        )
-    )
-    sys.path.append(str((Path(__file__).parent).resolve()))
+    yield monkeypatch
+
+
+@pytest.fixture()
+def wrapper_patch_no_sls(monkeypatch):
+    monkeypatch.setenv("LAMBDA_TASK_ROOT", str(Path(__file__).parent.resolve()))
+    monkeypatch.delenv("SLS_ORG_ID", raising=False)
+    monkeypatch.setenv("SLS_SDK_DEBUG", "1")
+    monkeypatch.setenv("LAMBDA_RUNTIME_DIR", "/var/runtime")
+    yield monkeypatch
+
+
+def test_exec_wrapper_succeeds(wrapper_patch, exec_wrapper_main):
+    # given
+    env = dict(os.environ)
+    wrapper_patch.setattr(os, "environ", env)
+    system_mock = MagicMock()
+    wrapper_patch.setattr(os, "system", system_mock)
+
+    lambda_handler = "success.handler"
+
+    wrapper_patch.setenv("_HANDLER", lambda_handler)
+
+    initial_sys_path = sys.path.copy()
 
     # when
-    from serverless_aws_lambda_sdk.internal_extension.exec_wrapper import (
-        main as exec_wrapper_main,
-    )
-
     exec_wrapper_main()
 
     # then
-    assert os.environ["_TEST_INTERNAL_EXTENSION"] == "1"
-    del os.environ["_TEST_INTERNAL_EXTENSION"]
+    assert os.environ["_ORIGIN_HANDLER"] == lambda_handler
+    assert (
+        os.environ["_HANDLER"]
+        == "serverless_aws_lambda_sdk.internal_extension.wrapper.handler"
+    )
+    assert sys.path == initial_sys_path
+    system_mock.assert_called_once()
 
 
-def main():
-    os.environ["_TEST_INTERNAL_EXTENSION"] = "1"
+def test_exec_wrapper_noops_if_sls_env_variable_is_missing(
+    wrapper_patch_no_sls,
+    exec_wrapper_main,
+):
+    # given
+    env = dict(os.environ)
+    wrapper_patch_no_sls.setattr(os, "environ", env)
+    system_mock = MagicMock()
+    wrapper_patch_no_sls.setattr(os, "system", system_mock)
+
+    lambda_handler = "success.handler"
+
+    wrapper_patch_no_sls.setenv("_HANDLER", lambda_handler)
+
+    initial_sys_path = sys.path.copy()
+
+    # when
+    exec_wrapper_main()
+
+    # then
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert sys.path == initial_sys_path
+    system_mock.assert_called_once()
+
+
+def test_exec_wrapper_noops_if_handler_not_specified(wrapper_patch, exec_wrapper_main):
+    # given
+    env = dict(os.environ)
+    wrapper_patch.setattr(os, "environ", env)
+    system_mock = MagicMock()
+    wrapper_patch.setattr(os, "system", system_mock)
+
+    lambda_handler = "success"
+
+    wrapper_patch.setenv("_HANDLER", lambda_handler)
+
+    initial_sys_path = sys.path.copy()
+
+    # when
+    exec_wrapper_main()
+
+    # then
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert sys.path == initial_sys_path
+    system_mock.assert_called_once()
+
+
+def test_exec_wrapper_noops_if_builtin_module(wrapper_patch, exec_wrapper_main):
+    # given
+    env = dict(os.environ)
+    wrapper_patch.setattr(os, "environ", env)
+    system_mock = MagicMock()
+    wrapper_patch.setattr(os, "system", system_mock)
+
+    lambda_handler = "builtins.print"
+
+    wrapper_patch.setenv("_HANDLER", lambda_handler)
+
+    initial_sys_path = sys.path.copy()
+
+    # when
+    exec_wrapper_main()
+
+    # then
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert sys.path == initial_sys_path
+    system_mock.assert_called_once()
+
+
+def test_exec_wrapper_noops_if_module_does_not_exist(wrapper_patch, exec_wrapper_main):
+    # given
+    env = dict(os.environ)
+    wrapper_patch.setattr(os, "environ", env)
+    system_mock = MagicMock()
+    wrapper_patch.setattr(os, "system", system_mock)
+
+    lambda_handler = "nonexistent.module"
+
+    wrapper_patch.setenv("_HANDLER", lambda_handler)
+
+    initial_sys_path = sys.path.copy()
+
+    # when
+    exec_wrapper_main()
+
+    # then
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert sys.path == initial_sys_path
+    system_mock.assert_called_once()
--- python/packages/aws-lambda-sdk/tests/internal_extension/test_internal_extension_wrapper.py
+++ python/packages/aws-lambda-sdk/tests/internal_extension/test_internal_extension_wrapper.py
@@ -1,231 +1,89 @@
-from __future__ import annotations
-import os
-from typing_extensions import Final
-from pathlib import Path
-
-from .. import context
-from ..fixtures import (
-    SUBMODULE_HANDLER,
-    SUCCESS_HANDLER,
-    ERROR_HANDLER,
-    UNHANDLED_ERROR_HANDLER,
-    UNIMPORTABLE_HANDLER,
-    SYNTAX_ERROR_HANDLER,
-)
-import importlib
 import pytest
-from pytest_httpserver import HTTPServer
-from werkzeug.wrappers import Response
+import os
+from unittest.mock import patch
 
 
-HANDLER_MODULE_DIR: Final[str] = str(Path(__file__).parent.resolve())
-
-
-def test_raises_exception_when_handler_is_not_set(reset_sdk):
+def test_exec_wrapper_succeeds(reset_sdk, monkeypatch):
     # given
-    from serverless_aws_lambda_sdk.exceptions import HandlerNotFound
-
     env = dict(os.environ)
-    reset_sdk.setattr(os, "environ", env)
+    monkeypatch.setattr(os, "environ", env)
+
+    lambda_handler = "success.handler"
+    monkeypatch.setenv("_ORIGIN_HANDLER", lambda_handler)
 
     # when
-    with pytest.raises(HandlerNotFound):
-        from serverless_aws_lambda_sdk.internal_extension import wrapper
+    import serverless_aws_lambda_sdk.internal_extension.wrapper as wrapper
 
-        importlib.reload(wrapper)
-
-
-def test_raises_exception_when_handler_function_does_not_exist(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = dict(os.environ)
-    env[Env.HANDLER] = f"{SUCCESS_HANDLER}.invalid"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{SUCCESS_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{SUCCESS_HANDLER}.invalid"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "invalid"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    with pytest.raises(Exception):
-        from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-        importlib.reload(wrapper)
-
-
-def test_raises_exception_when_handler_module_has_an_error(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = dict(os.environ)
-    env[Env.HANDLER] = f"{UNIMPORTABLE_HANDLER}.foo"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{UNIMPORTABLE_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{UNIMPORTABLE_HANDLER}.foo"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "foo"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    with pytest.raises(ImportError):
-        from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-        importlib.reload(wrapper)
-
-
-def test_raises_exception_when_handler_module_has_a_syntax_error(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = dict(os.environ)
-    env[Env.HANDLER] = f"{SYNTAX_ERROR_HANDLER}.foo"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{SYNTAX_ERROR_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{SYNTAX_ERROR_HANDLER}.foo"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "foo"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    with pytest.raises(SyntaxError):
-        from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-        importlib.reload(wrapper)
-
-
-def test_can_instrument_handler(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = os.environ
-    env[Env.HANDLER] = f"{SUCCESS_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{SUCCESS_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{SUCCESS_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "handler"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-    importlib.reload(wrapper)
-    response = wrapper.handler({}, context)
+    response = wrapper.handler({}, {})
 
     # then
-    assert response == "ok", "handler return should not be tampered with"
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert os.environ.get("_ORIGIN_HANDLER") is None
+    assert response == "ok"
 
 
-def test_can_instrument_handler_dev_mode_without_running_server(reset_sdk_dev_mode):
+def test_exec_wrapper_nonexistent_handler(reset_sdk, monkeypatch):
     # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
+    env = dict(os.environ)
+    monkeypatch.setattr(os, "environ", env)
 
-    env = os.environ
-    env[Env.HANDLER] = f"{SUCCESS_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{SUCCESS_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{SUCCESS_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "handler"
-    reset_sdk_dev_mode.setattr(os, "environ", env)
+    lambda_handler = "success.nonexistent_handler"
+    monkeypatch.setenv("_ORIGIN_HANDLER", lambda_handler)
 
     # when
-    from serverless_aws_lambda_sdk.internal_extension import wrapper
+    import serverless_aws_lambda_sdk.internal_extension.wrapper as wrapper
 
-    responses = [wrapper.handler({}, context), wrapper.handler({}, context)]
+    with pytest.raises(wrapper.HandlerNotFound):
+        wrapper.handler({}, {})
 
     # then
-    assert responses == ["ok", "ok"], "handler return should not be tampered with"
+    assert callable(wrapper.handler)
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert os.environ.get("_ORIGIN_HANDLER") is None
 
 
-def test_can_instrument_handler_dev_mode_with_running_server(
-    reset_sdk_dev_mode,
-    httpserver_listen_address,
-    httpserver: HTTPServer,
+def test_exec_wrapper_noncallable_handler(reset_sdk, monkeypatch):
+    # given
+    env = dict(os.environ)
+    monkeypatch.setattr(os, "environ", env)
+
+    lambda_handler = "not_callable.handler"
+    monkeypatch.setenv("_ORIGIN_HANDLER", lambda_handler)
+
+    # when
+    import serverless_aws_lambda_sdk.internal_extension.wrapper as wrapper
+
+    with pytest.raises(wrapper.HandlerTypeError):
+        wrapper.handler({}, {})
+
+    # then
+    assert callable(wrapper.handler)
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert os.environ.get("_ORIGIN_HANDLER") is None
+
+
+def test_exec_wrapper_returns_uninstrumented_handler_if_initialization_error(
+    reset_sdk, monkeypatch
 ):
     # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
+    env = dict(os.environ)
+    monkeypatch.setattr(os, "environ", env)
 
-    env = os.environ
-    env[Env.HANDLER] = f"{SUCCESS_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{SUCCESS_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{SUCCESS_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "handler"
-    reset_sdk_dev_mode.setattr(os, "environ", env)
-
-    def http_handler(request):
-        return Response(str("OK"))
-
-    httpserver.expect_request("/request-response").respond_with_handler(http_handler)
-    httpserver.expect_request("/trace").respond_with_handler(http_handler)
+    lambda_handler = "success.handler"
+    monkeypatch.setenv("_ORIGIN_HANDLER", lambda_handler)
 
     # when
-    from serverless_aws_lambda_sdk.internal_extension import wrapper
+    import serverless_aws_lambda_sdk.instrument
 
-    responses = [wrapper.handler({}, context), wrapper.handler({}, context)]
+    with patch(
+        "serverless_aws_lambda_sdk.instrument.Instrumenter"
+    ) as mocked_instrumenter:
+        mocked_instrumenter.side_effect = Exception("error")
+        import serverless_aws_lambda_sdk.internal_extension.wrapper as wrapper
+
+        response = wrapper.handler({}, {})
 
     # then
-    assert responses == ["ok", "ok"], "handler return should not be tampered with"
-
-
-def test_can_instrument_handler_with_submodule(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = os.environ
-    env[Env.HANDLER] = f"{SUBMODULE_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{SUBMODULE_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{SUBMODULE_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "handler"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-    importlib.reload(wrapper)
-    response = wrapper.handler({}, context)
-
-    # then
-    assert response == "ok", "handler return should not be tampered with"
-
-
-def test_can_instrument_handler_when_handler_fails(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = os.environ
-    env[Env.HANDLER] = f"{ERROR_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{ERROR_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{ERROR_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "handler"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-    importlib.reload(wrapper)
-
-    with pytest.raises(Exception, match=r"Stop"):
-        wrapper.handler({}, context)
-
-
-def test_can_instrument_handler_when_handler_exits_with_error(reset_sdk):
-    # given
-    from serverless_aws_lambda_sdk.internal_extension.base import Env
-
-    env = os.environ
-    env[Env.HANDLER] = f"{UNHANDLED_ERROR_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_BASENAME] = f"{UNHANDLED_ERROR_HANDLER}"
-    env[Env.HANDLER_BASENAME] = f"{UNHANDLED_ERROR_HANDLER}.handler"
-    env[Env.HANDLER_MODULE_DIR] = HANDLER_MODULE_DIR
-    env[Env.HANDLER_FUNCTION_NAME] = "handler"
-    reset_sdk.setattr(os, "environ", env)
-
-    # when
-    from serverless_aws_lambda_sdk.internal_extension import wrapper
-
-    importlib.reload(wrapper)
-
-    # then
-    with pytest.raises(SystemExit, match=r"1"):
-        wrapper.handler({}, context)
+    assert os.environ["_HANDLER"] == lambda_handler
+    assert os.environ.get("_ORIGIN_HANDLER") is None
+    assert response == "ok"
