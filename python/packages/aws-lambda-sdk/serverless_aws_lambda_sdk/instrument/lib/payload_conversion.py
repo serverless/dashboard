5061bb0eb5ced75f0d8a2402a0d9f82016979bcb
Selçuk Cihan <selcukcihan@gmail.com>
2023-04-06 11:10:55 +0300

    fix: Fix generated span ids (#612)

--- python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/instrument/lib/payload_conversion.py
+++ python/packages/aws-lambda-sdk/serverless_aws_lambda_sdk/instrument/lib/payload_conversion.py
@@ -3,7 +3,22 @@ from google.protobuf import json_format
 
 
 def to_trace_payload(payload_dct: dict) -> TracePayload:
-    return json_format.ParseDict(payload_dct, TracePayload())
+    spans = payload_dct["spans"]
+    events = payload_dct["events"]
+    payload = json_format.ParseDict(payload_dct, TracePayload())
+    for index, span in enumerate(payload.spans):
+        span.id = bytes(spans[index]["id"], "utf-8")
+        span.trace_id = bytes(spans[index]["traceId"], "utf-8")
+        if spans[index]["parentSpanId"]:
+            span.parent_span_id = bytes(spans[index]["parentSpanId"], "utf-8")
+    for index, event in enumerate(payload.events):
+        event.id = bytes(events[index]["id"], "utf-8")
+        if events[index]["spanId"]:
+            event.span_id = bytes(events[index]["spanId"], "utf-8")
+        if events[index]["traceId"]:
+            event.trace_id = bytes(events[index]["traceId"], "utf-8")
+
+    return payload
 
 
 def to_request_response_payload(payload_dct: dict) -> RequestResponse:
