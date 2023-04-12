dda2409f7614f7064c332b32d56a1e9d7d7d4e64
Selçuk Cihan <selcukcihan@gmail.com>
2023-04-12 23:03:26 +0300

    [Python Lambda SDK] Instrument http requests (#630)
    * Instrument http requests
    
    * Add unit tests
    
    * Fix tests and path
    
    * Instrument asyncio
    
    * Handle target module not being installed
    
    * Increase test coverage
    
    * Unpack arguments better
    
    * Capture input/output
    
    * Use scheme from url
    
    * Handle the case where aiohttp version is incompatible
    
    * Ignore sls requests
    
    * Add documentation for http
    
    * Use internal sdk
    
    * Include port as part of host
    
    * Document supported clients
    
    * Ignore telemetry requests when instrumenting http
    
    * Add integration tests for http instrumentation
    
    * Upgrade base sdk dependency version
    
    * Update docs to add http instrumentation

---
+++ python/packages/aws-lambda-sdk/tests/fixtures/lambdas/aiohttp_requester.py
@@ -0,0 +1,65 @@
+import time
+import asyncio
+import sys
+from pathlib import Path
+
+
+TEST_SERVER_PORT = 3177
+httpd = None
+
+
+def run_http_server():
+    global httpd, TEST_SERVER_PORT
+    import http.server
+    import socketserver
+
+    class MyTCPServer(socketserver.TCPServer):
+        allow_reuse_address = True
+
+    Handler = http.server.SimpleHTTPRequestHandler
+    with MyTCPServer(("", TEST_SERVER_PORT), Handler) as httpd:
+        httpd.serve_forever(0.1)
+
+
+def make_http_request(url, use_ssl=False):
+    sys.path.append(Path(__file__).parent / "test_dependencies")
+    import aiohttp
+
+    async def _request():
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers={"someHeader": "bar"}) as resp:
+                print(resp.status)
+                print(await resp.text())
+
+    asyncio.run(_request())
+    sys.path.pop()
+
+
+def handler(event, context) -> str:
+    url = event.get("url")
+    if url:
+        from urllib.parse import urlparse
+
+        make_http_request(url)
+    else:
+        from threading import Thread
+
+        server_thread = Thread(target=run_http_server)
+        server_thread.start()
+
+        for i in range(10):
+            time.sleep(0.1)
+            try:
+                make_http_request(f"http://127.0.0.1:{TEST_SERVER_PORT}/?foo=bar")
+                break
+            except ConnectionRefusedError:
+                print(f"client: connection refused on {TEST_SERVER_PORT}")
+
+        if httpd:
+            httpd.shutdown()
+        server_thread.join()
+    return "ok"
+
+
+if __name__ == "__main__":
+    handler({}, None)
---
+++ python/packages/aws-lambda-sdk/tests/fixtures/lambdas/http_requester.py
@@ -0,0 +1,67 @@
+import time
+
+TEST_SERVER_PORT = 3177
+httpd = None
+
+
+def run_http_server():
+    global httpd, TEST_SERVER_PORT
+    import http.server
+    import socketserver
+
+    class MyTCPServer(socketserver.TCPServer):
+        allow_reuse_address = True
+
+    Handler = http.server.SimpleHTTPRequestHandler
+    with MyTCPServer(("", TEST_SERVER_PORT), Handler) as httpd:
+        httpd.serve_forever(0.1)
+
+
+def make_http_request(host, path, use_ssl=False):
+    import http.client
+
+    conn = None
+
+    try:
+        if use_ssl:
+            conn = http.client.HTTPSConnection(host)
+        else:
+            conn = http.client.HTTPConnection(host)
+        conn.request("GET", path, headers={"someHeader": "bar"})
+        conn.getresponse()
+    finally:
+        if conn:
+            conn.close()
+
+
+def handler(event, context) -> str:
+    url = event.get("url")
+    if url:
+        from urllib.parse import urlparse
+
+        parsed = urlparse(url)
+        make_http_request(
+            parsed.netloc, url.split(parsed.netloc)[1], use_ssl=parsed.scheme == "https"
+        )
+    else:
+        from threading import Thread
+
+        server_thread = Thread(target=run_http_server)
+        server_thread.start()
+
+        for i in range(10):
+            time.sleep(0.1)
+            try:
+                make_http_request(f"127.0.0.1:{TEST_SERVER_PORT}", "/?foo=bar")
+                break
+            except ConnectionRefusedError:
+                print(f"client: connection refused on {TEST_SERVER_PORT}")
+
+        if httpd:
+            httpd.shutdown()
+        server_thread.join()
+    return "ok"
+
+
+if __name__ == "__main__":
+    handler({}, None)
