4add1cae2fc050e247640b88c1dbb2d4a58f6317
Mariusz Nowak <medyk@medikoo.com>
2023-04-17 20:02:53 +0200

    test: Bump initialization threshold

--- node/packages/aws-lambda-sdk/test/benchmark/performance.test.js
+++ node/packages/aws-lambda-sdk/test/benchmark/performance.test.js
@@ -21,7 +21,7 @@ describe('performance', function () {
   // TODO: Reduce acceptable durations once improvements are made
   it('should introduce reasonable initialization overhead', () => {
     expect(results.get('internal').results.initialization.total.median).to.be.below(260);
-    expect(results.get('internalAndExternal').results.initialization.total.median).to.be.below(340);
+    expect(results.get('internalAndExternal').results.initialization.total.median).to.be.below(360);
   });
 
   it('should introduce reasonable first invocation overhead', () => {
