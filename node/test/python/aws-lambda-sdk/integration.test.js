572e1dc132e0a7deba0c6b8715bf2f19b20cf54c
Selcuk Cihan <selcukcihan@gmail.com>
2023-04-20 23:00:11 +0300

    Support python 3.10 in CI/CD

--- node/test/python/aws-lambda-sdk/integration.test.js
+++ node/test/python/aws-lambda-sdk/integration.test.js
@@ -593,6 +593,7 @@ describe('Python: integration', function () {
         variants: new Map([
           ['v3-8', { configuration: { Runtime: 'python3.8' } }],
           ['v3-9', { configuration: { Runtime: 'python3.9' } }],
+          ['v3-10', { configuration: { Runtime: 'python3.10' } }],
           [
             'sampled',
             {
@@ -616,6 +617,7 @@ describe('Python: integration', function () {
         variants: new Map([
           ['v3-8', { configuration: { Runtime: 'python3.8' } }],
           ['v3-9', { configuration: { Runtime: 'python3.9' } }],
+          ['v3-10', { configuration: { Runtime: 'python3.10' } }],
         ]),
         config: { expectedOutcome: 'error:handled' },
       },
@@ -626,6 +628,7 @@ describe('Python: integration', function () {
         variants: new Map([
           ['v3-8', { configuration: { Runtime: 'python3.8' } }],
           ['v3-9', { configuration: { Runtime: 'python3.9' } }],
+          ['v3-10', { configuration: { Runtime: 'python3.10' } }],
         ]),
         config: { expectedOutcome: 'error:unhandled' },
       },
@@ -962,6 +965,7 @@ describe('Python: integration', function () {
         variants: new Map([
           ['v3-8', { configuration: { Runtime: 'python3.8' } }],
           ['v3-9', { configuration: { Runtime: 'python3.9' } }],
+          ['v3-10', { configuration: { Runtime: 'python3.10' } }],
           ['dev-mode', devModeConfiguration],
         ]),
         config: sdkTestConfig,
@@ -973,6 +977,7 @@ describe('Python: integration', function () {
         variants: new Map([
           ['v3-8', { configuration: { Runtime: 'python3.8' } }],
           ['v3-9', { configuration: { Runtime: 'python3.9' } }],
+          ['v3-10', { configuration: { Runtime: 'python3.10' } }],
         ]),
       },
     ],
