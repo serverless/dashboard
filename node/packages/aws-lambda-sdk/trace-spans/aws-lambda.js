9ecade774f727f8d7e8b56c8c5454cb02f57bc9f
Mariusz Nowak <medyk@medikoo.com>
2023-04-21 16:32:15 +0200

    refactor: Remove no longer supported option

--- node/packages/aws-lambda-sdk/trace-spans/aws-lambda.js
+++ node/packages/aws-lambda-sdk/trace-spans/aws-lambda.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const TraceSpan = require('@serverless/sdk/lib/trace-span');
-const reportWarning = require('@serverless/sdk/lib/report-warning');
 
 const immutableTags = {
   'aws.lambda.name': process.env.AWS_LAMBDA_FUNCTION_NAME,
@@ -29,7 +28,6 @@ const awsLambdaSpan = new TraceSpan('aws.lambda', {
   startTime: EvalError.$serverlessAwsLambdaInitializationStartTime,
   immediateDescendants: ['aws.lambda.initialization'],
   tags: immutableTags,
-  _reportWarning: reportWarning,
 });
 
 if (process.env.AWS_LAMBDA_INITIALIZATION_TYPE === 'on-demand') {
