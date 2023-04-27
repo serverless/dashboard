820ea2678217253e9a62c1dd27d0d89161442b54
Selcuk Cihan <selcukcihan@gmail.com>
2023-04-27 15:31:39 +0300

    Reproduce and fix ddb tag issue

--- python/packages/aws-lambda-sdk/tests/instrumentation/test_aws_sdk.py
+++ python/packages/aws-lambda-sdk/tests/instrumentation/test_aws_sdk.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 import pytest
 import boto3
 import botocore
-from moto import mock_s3
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute
+from moto import mock_s3, mock_dynamodb
 import json
+from unittest.mock import MagicMock
 
 
 @pytest.fixture()
@@ -120,3 +123,91 @@ def test_aws_sdk_instrumentation_error(instrumenter):
     assert sdk_span.tags["aws.sdk.request_id"] is not None
     assert sdk_span.input is None
     assert sdk_span.output is None
+
+
+@mock_dynamodb
+def test_aws_sdk_instrumentation_of_dynamodb(instrumenter, monkeypatch):
+    # given
+    mock_report_error = MagicMock()
+    import sls_sdk.lib.tags
+
+    monkeypatch.setattr(sls_sdk.lib.tags, "report_error", mock_report_error)
+    table_name = "test-table"
+
+    class LocationModel(Model):
+        class Meta:
+            table_name = "test-table"
+
+        country = UnicodeAttribute(hash_key=True)
+        city = UnicodeAttribute(range_key=True)
+
+    dynamodb = boto3.client("dynamodb", region_name="us-east-1")
+    dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {"AttributeName": "country", "KeyType": "HASH"},
+            {"AttributeName": "city", "KeyType": "RANGE"},
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "country", "AttributeType": "S"},
+            {"AttributeName": "city", "AttributeType": "S"},
+        ],
+        BillingMode="PAY_PER_REQUEST",
+    )
+
+    # when
+    dynamodb.put_item(
+        TableName=table_name,
+        Item={"country": {"S": "France"}, "city": {"S": "Paris"}},
+    )
+    dynamodb.query(
+        TableName=table_name,
+        KeyConditionExpression="#country = :country",
+        ExpressionAttributeNames={"#country": "country"},
+        ExpressionAttributeValues={":id": {"S": "France"}},
+    )
+
+    res = boto3.resource("dynamodb", region_name="us-east-1")
+    from boto3.dynamodb.conditions import Key
+
+    list(
+        res.meta.client.get_paginator("query").paginate(
+            TableName=table_name,
+            KeyConditionExpression=Key("country").eq("France"),
+            FilterExpression=Key("city").eq("Paris"),
+            ProjectionExpression="country, city",
+        )
+    )
+
+    locations = [l for l in LocationModel.query("France")]
+    dynamodb.delete_table(TableName=table_name)
+
+    # then
+    mock_report_error.assert_not_called()
+    dynamodb_spans = [
+        s for s in instrumenter.trace_spans.root.spans if s.name.startswith("aws.sdk")
+    ]
+    assert [(l.country, l.city) for l in locations] == [("France", "Paris")]
+    [
+        "aws.sdk.dynamodb.createtable",
+        "aws.sdk.dynamodb.putitem",
+        "aws.sdk.dynamodb.query",
+        "aws.sdk.dynamodb.query",
+        "aws.sdk.dynamodb.deletetable",
+    ] == [s.name for s in dynamodb_spans]
+    for span in dynamodb_spans:
+        assert span.tags["aws.sdk.service"] == "dynamodb"
+        assert span.tags["aws.sdk.dynamodb.table_name"] == table_name
+
+    (query1_span, query2_span) = dynamodb_spans[2:4]
+    assert query1_span.tags["aws.sdk.dynamodb.key_condition"] == "#country = :country"
+
+    assert (
+        query2_span.tags["aws.sdk.dynamodb.key_condition"]
+        == "{'format': '{0} {operator} {1}', 'operator': '=', 'values': ['country', 'France']}"
+    )
+    assert query2_span.tags["aws.sdk.dynamodb.projection"] == "country, city"
+    assert (
+        query2_span.tags["aws.sdk.dynamodb.filter"]
+        == "{'format': '{0} {operator} {1}', 'operator': '=', 'values': ['city', 'Paris']}"
+    )
