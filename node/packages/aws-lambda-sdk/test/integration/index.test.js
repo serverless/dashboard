1ed3c4a9d7c29ad9d4ff045de124dd1081668737
Mariusz Nowak <medyk@medikoo.com>
2023-04-27 20:42:06 +0200

    test: Test parametrized express paths

--- node/packages/aws-lambda-sdk/test/integration/index.test.js
+++ node/packages/aws-lambda-sdk/test/integration/index.test.js
@@ -75,6 +75,12 @@ describe('integration', function () {
       Target: `integrations/${integrationId}`,
     });
 
+    await awsRequest(ApiGatewayV2, 'createRoute', {
+      ApiId: apiId,
+      RouteKey: 'POST /users/{user}/books/{book}',
+      Target: `integrations/${integrationId}`,
+    });
+
     await awsRequest(ApiGatewayV2, 'createStage', {
       ApiId: apiId,
       StageName: '$default',
@@ -1713,6 +1719,61 @@ describe('integration', function () {
                   }
                   expect(routerSpan.name).to.equal('express.middleware.router');
                   expect(String(routerSpan.parentSpanId)).to.equal(String(topRouterSpan.id));
+                  expect(routeSpan.name).to.equal('express.middleware.route.post.anonymous');
+                  expect(String(routeSpan.parentSpanId)).to.equal(String(routerSpan.id));
+                }
+              },
+            },
+          ],
+          [
+            'parametrized',
+            {
+              invoke: resolveExpressInvoke({ pathname: '/users/123/books/456' }),
+              test: ({ invocationsData, testConfig }) => {
+                for (const [
+                  index,
+                  {
+                    trace: { spans },
+                  },
+                ] of invocationsData.entries()) {
+                  const lambdaSpan = spans.shift();
+                  if (!index) spans.shift();
+                  const { tags: lambdaTags } = lambdaSpan;
+
+                  expect(lambdaTags.aws.lambda.eventSource).to.equal('aws.apigateway');
+                  expect(lambdaTags.aws.lambda.eventType).to.equal('aws.apigatewayv2.http.v2');
+
+                  expect(lambdaTags.aws.lambda.apiGateway).to.have.property('accountId');
+                  expect(lambdaTags.aws.lambda.apiGateway.apiId).to.equal(testConfig.apiId);
+                  expect(lambdaTags.aws.lambda.apiGateway.apiStage).to.equal('$default');
+                  expect(lambdaTags.aws.lambda.apiGateway.request).to.have.property('id');
+                  expect(lambdaTags.aws.lambda.apiGateway.request).to.have.property('timeEpoch');
+                  expect(lambdaTags.aws.lambda.http).to.have.property('host');
+                  expect(lambdaTags.aws.lambda.http).to.have.property('requestHeaderNames');
+                  expect(lambdaTags.aws.lambda.http.method).to.equal('POST');
+                  expect(lambdaTags.aws.lambda.http.path).to.equal('/users/123/books/456');
+
+                  expect(lambdaTags.aws.lambda.http.statusCode.toString()).to.equal('200');
+
+                  expect(lambdaTags.aws.lambda.httpRouter.path.toString()).to.equal(
+                    '/users/:userId/books/:bookId'
+                  );
+
+                  const [invocationSpan, expressSpan, ...middlewareSpans] = spans;
+                  const routeSpan = middlewareSpans.pop();
+                  const routerSpan = middlewareSpans[middlewareSpans.length - 1];
+
+                  expect(expressSpan.parentSpanId).to.deep.equal(invocationSpan.id);
+
+                  expect(middlewareSpans.map(({ name }) => name)).to.deep.equal([
+                    'express.middleware.query',
+                    'express.middleware.expressinit',
+                    'express.middleware.jsonparser',
+                    'express.middleware.router',
+                  ]);
+                  for (const middlewareSpan of middlewareSpans) {
+                    expect(String(middlewareSpan.parentSpanId)).to.equal(String(expressSpan.id));
+                  }
                   expect(routeSpan.name).to.equal('express.middleware.route.post.anonymous');
                   expect(String(routeSpan.parentSpanId)).to.equal(String(routerSpan.id));
                 }
