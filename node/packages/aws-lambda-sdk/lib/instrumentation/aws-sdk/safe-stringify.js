03b3161eae56b67bb37705c52c987571719f8b61
Mariusz Nowak <medyk@medikoo.com>
2023-05-12 17:33:42 +0200

    feat: Placeholder stream values when reporting request/response body

--- node/packages/aws-lambda-sdk/lib/instrumentation/aws-sdk/safe-stringify.js
+++ node/packages/aws-lambda-sdk/lib/instrumentation/aws-sdk/safe-stringify.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const util = require('util');
+const isObject = require('type/object/is');
+
+// Credit: https://github.com/sindresorhus/is-stream/blob/6913e344ab2dd63041bb7c03095876ce5a7e0a8b/index.js#L1-L5
+const isStream = (value) => isObject(value) && typeof value.pipe === 'function';
 
 const replacer = (key, value) => {
   if (typeof value === 'bigint') return value.toString();
+  if (isStream(value)) return '<stream>';
   return value;
 };
 
