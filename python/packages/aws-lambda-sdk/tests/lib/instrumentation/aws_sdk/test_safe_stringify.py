5643610f826513686e003c5d0743019afeda3a18
Selcuk Cihan <selcukcihan@gmail.com>
2023-05-12 11:09:27 +0300

    Handle datetime json serialization

--- python/packages/aws-lambda-sdk/tests/lib/instrumentation/aws_sdk/test_safe_stringify.py
+++ python/packages/aws-lambda-sdk/tests/lib/instrumentation/aws_sdk/test_safe_stringify.py
@@ -1,6 +1,7 @@
 from serverless_aws_lambda_sdk.lib.instrumentation.aws_sdk.safe_stringify import (
     safe_stringify,
 )
+from datetime import datetime
 
 
 def test_safe_stringify_success():
@@ -16,10 +17,22 @@ def test_safe_stringify_success():
 
 def test_safe_stringify_failure():
     # given
-    input = {"foo": object()}
+    input = {object(): object()}
 
     # when
     result = safe_stringify(input)
 
     # then
     assert result is None
+
+
+def test_safe_stringify_with_datetime():
+    # given
+    now = datetime.now()
+    input = {"timestamp": now}
+
+    # when
+    result = safe_stringify(input)
+
+    # then
+    assert result == f'{{"timestamp": "{str(now)}"}}'
