9b3fd8c2ea6cc43881694ee648b3ea2590e40c87
Mariusz Nowak <medyk@medikoo.com>
2023-05-12 16:09:21 +0200

    feat: Do not write dev mode specific events in trace payload

--- node/packages/aws-lambda-sdk/instrument/lib/filter-captured-event.js
+++ node/packages/aws-lambda-sdk/instrument/lib/filter-captured-event.js
@@ -1,6 +1,21 @@
 'use strict';
 
+const serverlessSdk = require('./sdk');
+
+const devModeSpecificEventFingerprints = new Set([
+  'INPUT_BODY_BINARY',
+  'INPUT_BODY_TOO_LARGE',
+  'OUTPUT_BODY_BINARY',
+  'OUTPUT_BODY_TOO_LARGE',
+]);
+
 module.exports = (capturedEvent) => {
+  if (
+    !serverlessSdk._isDevMode &&
+    devModeSpecificEventFingerprints.has(capturedEvent.customFingerprint)
+  ) {
+    return false;
+  }
   switch (capturedEvent.name) {
     case 'telemetry.error.generated.v1':
       // Skip on errors logged via `console.error` in AWS Lambda runtime
