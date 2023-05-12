50166a0d821e4fc3b33ba6fa10c8391168ac76c1
Mariusz Nowak <medyk@medikoo.com>
2023-05-12 16:00:52 +0200

    refactor: Improve filter logic organization

--- node/packages/aws-lambda-sdk/instrument/lib/filter-captured-event.js
+++ node/packages/aws-lambda-sdk/instrument/lib/filter-captured-event.js
@@ -1,7 +1,11 @@
 'use strict';
 
 module.exports = (capturedEvent) => {
-  // Skip on errors logged via `console.error` in AWS Lambda runtime
-  if (capturedEvent.name !== 'telemetry.error.generated.v1') return true;
-  return !capturedEvent.tags.get('error.stacktrace').split('\n')[0].includes('/var/runtime/');
+  switch (capturedEvent.name) {
+    case 'telemetry.error.generated.v1':
+      // Skip on errors logged via `console.error` in AWS Lambda runtime
+      return !capturedEvent.tags.get('error.stacktrace').split('\n')[0].includes('/var/runtime/');
+    default:
+      return true;
+  }
 };
