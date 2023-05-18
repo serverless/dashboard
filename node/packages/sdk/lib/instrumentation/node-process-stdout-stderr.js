921817858687aa1438c7ea7e6b2a72b1cf949c26
Mariusz Nowak <medyk@medikoo.com>
2023-05-18 17:16:40 +0200

    fix: Ensure to expose our crashes that `console` will swallow

--- node/packages/sdk/lib/instrumentation/node-process-stdout-stderr.js
+++ node/packages/sdk/lib/instrumentation/node-process-stdout-stderr.js
@@ -19,12 +19,28 @@ module.exports.install = () => {
 
   nodeStdout.write = function (...args) {
     original.stdout.write.apply(this, args);
-    attemptParseStructuredLogAndCapture(args[0]);
+    try {
+      attemptParseStructuredLogAndCapture(args[0]);
+    } catch (error) {
+      process.nextTick(() => {
+        // Prevent crashes swalling by Node.js console
+        // see: https://github.com/nodejs/node/blob/3538e1bcde45eae60c6bdbedeb3f765dbb9714c2/lib/internal/console/constructor.js#L306-L310
+        throw error;
+      });
+    }
   };
 
   nodeStderr.write = function (...args) {
     original.stderr.write.apply(this, args);
-    attemptParseStructuredLogAndCapture(args[0]);
+    try {
+      attemptParseStructuredLogAndCapture(args[0]);
+    } catch (error) {
+      process.nextTick(() => {
+        // Prevent crashes swalling by Node.js console
+        // see: https://github.com/nodejs/node/blob/3538e1bcde45eae60c6bdbedeb3f765dbb9714c2/lib/internal/console/constructor.js#L306-L310
+        throw error;
+      });
+    }
   };
 
   uninstall = () => {
