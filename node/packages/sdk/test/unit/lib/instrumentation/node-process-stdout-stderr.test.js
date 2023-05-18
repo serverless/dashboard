6485921ce4022787bf0e522b951788690df1b4b4
Mariusz Nowak <medyk@medikoo.com>
2023-05-18 15:05:26 +0200

    test: Improve tests organization

--- node/packages/sdk/test/unit/lib/instrumentation/node-process-stdout-stderr.test.js
+++ node/packages/sdk/test/unit/lib/instrumentation/node-process-stdout-stderr.test.js
@@ -23,137 +23,143 @@ describe('lib/instrumentation/node-process-stdout-stderr.js', () => {
     delete require('uni-global')('serverless/sdk/202212').serverlessSdk;
   });
 
-  it('should instrument `process.stderr.write` with powertools logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+  describe('AWS Lambda Powertools', () => {
+    it('should instrument error', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
 
-    const logger = new PowertoolsLogger({
-      serviceName: 'test-app',
-      persistentLogAttributes: { testtag: 'test tag' },
+      const logger = new PowertoolsLogger({
+        serviceName: 'test-app',
+        persistentLogAttributes: { testtag: 'test tag' },
+      });
+
+      logger.error('Test Error', new Error('My error'));
+      expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
+      expect(capturedEvent.tags.get('error.message')).to.equal('My error');
+      expect(capturedEvent.customTags.get('service')).to.equal('test-app');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
+    });
+    it('should instrument warning', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+
+      const logger = new PowertoolsLogger({
+        serviceName: 'test-app',
+        persistentLogAttributes: { testtag: 'test tag' },
+      });
+
+      logger.warn('Test Warning');
+      expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
+      expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
+      expect(capturedEvent.customTags.get('service')).to.equal('test-app');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
+    });
+  });
+  describe('Pino', () => {
+    it('should instrument error', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+
+      const parent = pino();
+
+      const logger = parent.child({ testtag: 'test tag' });
+
+      logger.error(new Error('My error'));
+      expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
+      expect(capturedEvent.tags.get('error.message')).to.equal('My error');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
     });
 
-    logger.error('Test Error', new Error('My error'));
-    expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
-    expect(capturedEvent.tags.get('error.message')).to.equal('My error');
-    expect(capturedEvent.customTags.get('service')).to.equal('test-app');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
+    it('should instrument warning', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+
+      const parent = pino();
+
+      const logger = parent.child({ testtag: 'test tag' });
+
+      logger.warn('Test Warning');
+      expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
+      expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
+    });
   });
 
-  it('should instrument `process.stderr.write` with pino logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+  describe('Winston', () => {
+    it('should instrument error', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
 
-    const parent = pino();
+      const parent = winston.createLogger({
+        format: winston.format.json(),
+        transports: [new winston.transports.Console()],
+      });
 
-    const logger = parent.child({ testtag: 'test tag' });
+      const logger = parent.child({ testtag: 'test tag' });
 
-    logger.error(new Error('My error'));
-    expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
-    expect(capturedEvent.tags.get('error.message')).to.equal('My error');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
-  });
-
-  it('should instrument `process.stderr.write` with winston logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
-
-    const parent = winston.createLogger({
-      format: winston.format.json(),
-      transports: [new winston.transports.Console()],
+      const error = new Error('My error');
+      error.name = 'WinstonError';
+      logger.error('winston error', error);
+      expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
+      expect(capturedEvent.tags.get('error.message')).to.equal('winston error My error');
+      expect(capturedEvent.tags.get('error.name')).to.equal('WinstonError');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
     });
 
-    const logger = parent.child({ testtag: 'test tag' });
+    it('should instrument warning', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
 
-    const error = new Error('My error');
-    error.name = 'WinstonError';
-    logger.error('winston error', error);
-    expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
-    expect(capturedEvent.tags.get('error.message')).to.equal('winston error My error');
-    expect(capturedEvent.tags.get('error.name')).to.equal('WinstonError');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
+      const parent = winston.createLogger({
+        format: winston.format.json(),
+        transports: [new winston.transports.Console()],
+      });
+
+      const logger = parent.child({ testtag: 'test tag' });
+
+      logger.warn('Test Warning');
+      expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
+      expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
+    });
   });
 
-  it('should instrument `process.stderr.write` with bunyan logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+  describe('Bunyan', () => {
+    it('should instrument error', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
 
-    const parent = bunyan.createLogger({ name: 'bunyan-test' });
-    const logger = parent.child({ testtag: 'test tag' });
+      const parent = bunyan.createLogger({ name: 'bunyan-test' });
+      const logger = parent.child({ testtag: 'test tag' });
 
-    const error = new Error('My error');
-    error.name = 'BunyanError';
-    logger.error(error, 'bunyan error');
-    expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
-    expect(capturedEvent.tags.get('error.message')).to.equal('My error');
-    expect(capturedEvent.tags.get('error.name')).to.equal('BunyanError');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
-  });
-
-  it('should instrument `process.stdout.write` with powertools logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
-
-    const logger = new PowertoolsLogger({
-      serviceName: 'test-app',
-      persistentLogAttributes: { testtag: 'test tag' },
+      const error = new Error('My error');
+      error.name = 'BunyanError';
+      logger.error(error, 'bunyan error');
+      expect(capturedEvent.name).to.equal('telemetry.error.generated.v1');
+      expect(capturedEvent.tags.get('error.message')).to.equal('My error');
+      expect(capturedEvent.tags.get('error.name')).to.equal('BunyanError');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
     });
 
-    logger.warn('Test Warning');
-    expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
-    expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
-    expect(capturedEvent.customTags.get('service')).to.equal('test-app');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
-  });
+    it('should instrument warning', () => {
+      let capturedEvent;
+      serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
 
-  it('should instrument `process.stdout.write` with pino logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
+      const parent = bunyan.createLogger({ name: 'bunyan-test' });
+      const logger = parent.child({ testtag: 'test tag' });
 
-    const parent = pino();
-
-    const logger = parent.child({ testtag: 'test tag' });
-
-    logger.warn('Test Warning');
-    expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
-    expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
-  });
-
-  it('should instrument `process.stdout.write` with winston logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
-
-    const parent = winston.createLogger({
-      format: winston.format.json(),
-      transports: [new winston.transports.Console()],
+      logger.warn('Test Warning');
+      expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
+      expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
+      expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
+      expect(capturedEvent._origin).to.equal('nodeConsole');
     });
-
-    const logger = parent.child({ testtag: 'test tag' });
-
-    logger.warn('Test Warning');
-    expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
-    expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
-  });
-
-  it('should instrument `process.stdout.write` with bunyan logger', () => {
-    let capturedEvent;
-    serverlessSdk._eventEmitter.once('captured-event', (event) => (capturedEvent = event));
-
-    const parent = bunyan.createLogger({ name: 'bunyan-test' });
-    const logger = parent.child({ testtag: 'test tag' });
-
-    logger.warn('Test Warning');
-    expect(capturedEvent.name).to.equal('telemetry.warning.generated.v1');
-    expect(capturedEvent.tags.get('warning.message')).to.equal('Test Warning');
-    expect(capturedEvent.customTags.get('testtag')).to.equal('test tag');
-    expect(capturedEvent._origin).to.equal('nodeConsole');
   });
 });
