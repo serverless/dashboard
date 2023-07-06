f6fe1d788b1a2fb2e6b2d7590a7f3f2e7f547223
Max Marze <max@marze.dev>
2023-07-06 15:19:18 -0400

    fix: Instrument @smithy scoped smithy client

--- node/packages/aws-lambda-sdk/lib/instrumentation/aws-sdk/index.js
+++ node/packages/aws-lambda-sdk/lib/instrumentation/aws-sdk/index.js
@@ -2,9 +2,7 @@
 
 const cjsHook = require('@serverless/sdk/lib/instrumentation/utils/cjs-hook');
 const instrumentV2Sdk = require('../../../instrumentation/aws-sdk-v2').install;
-const instrumentV3Client = require('../../../instrumentation/aws-sdk-v3-client').install;
-
-const instrumentedv3SmithyClients = new WeakMap();
+const instrumentSmithyClient = require('../../../instrumentation/smithy-client').install;
 
 module.exports.install = () => {
   // AWS SDK v2
@@ -12,25 +10,11 @@ module.exports.install = () => {
 
   // AWS SDK v3
   cjsHook.register('/@aws-sdk/smithy-client/dist-cjs/client.js', ({ Client }) => {
-    if (instrumentedv3SmithyClients.has(Client)) return instrumentedv3SmithyClients.get(Client);
-    const originalSend = Client.prototype.send;
-    const uninstallers = new Set();
-    Client.prototype.send = function send(command, optionsOrCb, cb) {
-      try {
-        uninstallers.add(instrumentV3Client(this));
-      } catch (error) {
-        serverlessSdk._reportError(error);
-      }
-      return originalSend.call(this, command, optionsOrCb, cb);
-    };
-    const uninstall = () => {
-      if (!instrumentedv3SmithyClients.has(Client)) return;
-      for (const uninstallClient of uninstallers) uninstallClient();
-      Client.prototype.send = originalSend;
-      instrumentedv3SmithyClients.delete(Client);
-    };
-    instrumentedv3SmithyClients.set(Client, uninstall);
-    return uninstall;
+    return instrumentSmithyClient(Client);
+  });
+
+  cjsHook.register('/@smithy/smithy-client/dist-cjs/client.js', ({ Client }) => {
+    return instrumentSmithyClient(Client);
   });
 };
 
@@ -38,5 +22,3 @@ module.exports.uninstall = () => {
   cjsHook.unregister('/aws-sdk/lib/core.js');
   cjsHook.unregister('/@aws-sdk/smithy-client/dist-cjs/client.js');
 };
-
-const serverlessSdk = require('../../../');
