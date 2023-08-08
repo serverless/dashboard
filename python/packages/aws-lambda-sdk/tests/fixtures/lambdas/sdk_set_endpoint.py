1969695df24772c1979d814abf1bc8942f3c0a7f
Selcuk Cihan <selcukcihan@gmail.com>
2023-08-08 17:10:18 +0300

    Override http router path if endpoint was set

---
+++ python/packages/aws-lambda-sdk/tests/fixtures/lambdas/sdk_set_endpoint.py
@@ -0,0 +1,10 @@
+import json
+from serverless_sdk import serverlessSdk as sdk
+
+
+def handler(event, context) -> str:
+    sdk.set_endpoint("/test/set/endpoint")
+    return {
+        "statusCode": 200,
+        "body": json.dumps("ok"),
+    }
